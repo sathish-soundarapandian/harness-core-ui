c28de63a0572d7b887d0c8f73d802d4d36623553
Mayank Verma <mayank.verma@harness.io>
2022-04-11 19:09:44 +0530

    test: [PL-23322]: updated tests to use renderHook

--- src/framework/PreferenceStore/__tests__/PreferenceStoreContext.test.tsx
+++ src/framework/PreferenceStore/__tests__/PreferenceStoreContext.test.tsx
@@ -4,29 +4,84 @@
  * that can be found in the licenses directory at the root of this repository, also available at
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { waitFor } from '@testing-library/react'
+import { renderHook, act as actReactHooks } from '@testing-library/react-hooks'
+import { TestWrapper } from '@common/utils/testUtils'
+import routes from '@common/RouteDefinitions'
+import * as commonHooks from '@common/hooks/useLocalStorage'
+import { getKey, PreferenceScope, usePreferenceStore } from '../PreferenceStoreContext'
+const ENTITY_TO_SAVE = 'MySavedValue'
 
-import { getStringKeyFromObjectValues, PreferenceScope } from '../PreferenceStoreContext'
+// eslint-disable-next-line jest-no-mock
+jest.mock('react-router-dom', () => ({
+  ...(jest.requireActual('react-router-dom') as any),
+  useParams: jest.fn()
+}))
+
+const defaultUuid = '1234'
 
 describe('PreferenceStoreContext tests', () => {
-  test('if getStringKeyFromObjectValues works correctly', () => {
-    let key = getStringKeyFromObjectValues(PreferenceScope.USER, { userId: 'abc@gmail.com' }, 'MY_SAVED_ENTITY')
+  beforeEach(() => {
+    // eslint-disable-next-line
+    // @ts-ignore
+    useParams.mockImplementation(() => {
+      return { accountId: 'accountId', projectIdentifier: 'projectIdentifier', orgIdentifier: 'orgIdentifier' }
+    })
+  })
+
+  test('if renderhook works for usePreferenceStore', async () => {
+    const mockedSetVal = jest.fn()
+    // jest.spyOn(commonHooks, 'useLocalStorage').mockReturnValue([val, mockedSetVal])
+    jest
+      .spyOn(commonHooks, 'useLocalStorage')
+      .mockImplementationOnce(() => {
+        return ['ONE', mockedSetVal]
+      })
+      .mockImplementationOnce(() => {
+        return ['TWO', mockedSetVal]
+      })
+      .mockImplementationOnce(() => {
+        return [null, mockedSetVal]
+      })
+
+    const wrapper = ({ children }: React.PropsWithChildren<unknown>): React.ReactElement => (
+      <TestWrapper path={routes.toProjects({ accountId: defaultUuid })} pathParams={{ accountId: defaultUuid }}>
+        {children}
+      </TestWrapper>
+    )
+    const { result } = renderHook(() => usePreferenceStore<number>(PreferenceScope.MACHINE, ENTITY_TO_SAVE), {
+      wrapper
+    })
+
+    const { preference, setPreference, clearPreference, updatePreferenceStore } = result.current
+    actReactHooks(() => {
+      updatePreferenceStore({ currentUserInfo: { email: 'abc@gmail.com' } })
+    })
+    await waitFor(() => expect(preference).toBe(undefined))
+    actReactHooks(() => {
+      setPreference(55)
+    })
+    await waitFor(() => expect(mockedSetVal).toHaveBeenCalledTimes(1))
+    // rerender()
+    // jest.spyOn(commonHooks, 'useLocalStorage').mockReturnValue([55, mockedSetVal])
+    // await waitFor(() => expect(preference).toBe('TWO'))
+    actReactHooks(() => {
+      clearPreference()
+    })
+    await waitFor(() => expect(mockedSetVal).toHaveBeenCalledTimes(2))
+    // await waitFor(() => expect(savedVal).toBe('TWO'))
+  })
+
+  test('if getKey works correctly', () => {
+    let key = getKey(['abc@gmail.com'], 'MY_SAVED_ENTITY')
     expect(key).toBe('abc@gmail.com/MY_SAVED_ENTITY')
 
-    key = getStringKeyFromObjectValues(PreferenceScope.ACCOUNT, { accountId: 'abcXYZ' }, 'MY_SAVED_ENTITY')
-    expect(key).toBe('abcXYZ/MY_SAVED_ENTITY')
-
-    key = getStringKeyFromObjectValues(
-      PreferenceScope.ORG,
-      { accountId: 'abcXYZ', orgIdentifier: 'theOrgId' },
-      'MY_SAVED_ENTITY'
-    )
+    key = getKey(['abcXYZ', 'theOrgId'], 'MY_SAVED_ENTITY')
     expect(key).toBe('abcXYZ/theOrgId/MY_SAVED_ENTITY')
 
-    key = getStringKeyFromObjectValues(
-      PreferenceScope.PROJECT,
-      { accountId: 'abcXYZ', orgIdentifier: 'theOrgId', projectIdentifier: 'my_project' },
-      'MY_SAVED_ENTITY'
-    )
-    expect(key).toBe('abcXYZ/theOrgId/my_project/MY_SAVED_ENTITY')
+    key = getKey(['abcXYZ', undefined], 'MY_SAVED_ENTITY')
+    expect(key).toBe('abcXYZ//MY_SAVED_ENTITY')
   })
 })
