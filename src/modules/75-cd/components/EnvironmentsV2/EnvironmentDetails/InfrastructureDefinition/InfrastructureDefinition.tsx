59aa923d32e2492b66d551d350f05d46e95ce73c
Ashwin Bhatkal <ashwin.bhatkal@harness.io>
2022-06-20 07:36:52 +0530

    test: infrastructure definition

--- src/modules/75-cd/components/EnvironmentsV2/EnvironmentDetails/InfrastructureDefinition/InfrastructureDefinition.tsx
+++ src/modules/75-cd/components/EnvironmentsV2/EnvironmentDetails/InfrastructureDefinition/InfrastructureDefinition.tsx
@@ -18,16 +18,18 @@ import { useStrings } from 'framework/strings'
 import type { EnvironmentPathProps, ProjectPathProps } from '@common/interfaces/RouteInterfaces'
 import { ContainerSpinner } from '@common/components/ContainerSpinner/ContainerSpinner'
 
-import { InfrastructureList } from './InfrastructureList/InfrastructureList'
+import InfrastructureList from './InfrastructureList/InfrastructureList'
 import InfrastructureModal from './InfrastructureModal'
 
 import css from './InfrastructureDefinition.module.scss'
+import useRBACError from '@rbac/utils/useRBACError/useRBACError'
 
 export default function InfrastructureDefinition() {
   const { accountId, orgIdentifier, projectIdentifier, environmentIdentifier } = useParams<
     ProjectPathProps & EnvironmentPathProps
   >()
   const { getString } = useStrings()
+  const { getRBACErrorMessage } = useRBACError()
   const [infrastructureToEdit, setInfrastructureToEdit] = useState<string | undefined>()
 
   const { data, loading, error, refetch } = useGetInfrastructureList({
@@ -73,7 +75,7 @@ export default function InfrastructureDefinition() {
       {loading ? (
         <ContainerSpinner />
       ) : error ? (
-        <Page.Error>{error}</Page.Error>
+        <Page.Error>{getRBACErrorMessage(error)}</Page.Error>
       ) : (
         <>
           <Button
