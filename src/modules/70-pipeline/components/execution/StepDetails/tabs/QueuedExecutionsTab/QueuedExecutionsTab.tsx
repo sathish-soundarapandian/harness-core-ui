b5138adb6bcf5468f8bf41d96bb3957c30f4bec1
ishantmehtaharness <ishant@harness.io>
2022-06-21 00:22:03 +0530

    feat: [PIE-3871]: Makes pipeline name clickable

--- src/modules/70-pipeline/components/execution/StepDetails/tabs/QueuedExecutionsTab/QueuedExecutionsTab.tsx
+++ src/modules/70-pipeline/components/execution/StepDetails/tabs/QueuedExecutionsTab/QueuedExecutionsTab.tsx
@@ -6,17 +6,19 @@
  */
 
 import React from 'react'
+import { capitalize } from 'lodash-es'
 import classnames from 'classnames'
-import { useParams } from 'react-router-dom'
+import { useParams, NavLink } from 'react-router-dom'
 import moment from 'moment'
+import routes from '@common/RouteDefinitions'
 import { useStrings, StringKeys } from 'framework/strings'
+import type { Module } from 'framework/types/ModuleName'
 import {
   ExecutionNode,
-  ResourceConstraintDetail,
+  // ResourceConstraintDetail,
   ResponseResourceConstraintExecutionInfo,
   useGetResourceConstraintsExecutionInfo
 } from 'services/pipeline-ng'
-import type { ExecutionPathProps } from '@common/interfaces/RouteInterfaces'
 import stepDetailsTabCss from '../StepDetailsTab/StepDetailsTab.module.scss'
 import css from './QueuedExecutionsTab.module.scss'
 
@@ -27,42 +29,65 @@ export interface ExecutionStepDetailsTabProps {
 type getStringType = (key: StringKeys, vars?: Record<string, any>) => string
 
 const renderState = (getString: getStringType, state?: string, isCurrent?: boolean) => {
+  if (state === 'ACTIVE') {
+    return capitalize(getString('pipeline.executionStatus.Running'))
+  }
   if (isCurrent) {
     return getString('common.current')
   }
   if (state === 'BLOCKED') {
     return ''
   }
-  if (state === 'ACTIVE') {
-    return getString('pipeline.executionStatus.Running')
-  }
-  return state
+  return capitalize(state)
 }
 
 const renderData = (
   resourceConstraintsData: ResponseResourceConstraintExecutionInfo | null,
   getString: getStringType,
-  executionIdentifier?: string
+  params: Record<string, string>
 ) => {
   const resourceConstraints = resourceConstraintsData?.data?.resourceConstraints || []
   if (!resourceConstraints.length) {
     return 'Empty Data'
   }
+  const { executionIdentifier, orgIdentifier, projectIdentifier, pipelineIdentifier, accountId, module } = params
 
-  const startTs = +new Date()
   return (
     <>
       <div className={css.totalCount}>{getString('pipeline.totalCount', { count: resourceConstraints.length })}</div>
       <div className={css.queuedExecutionsList}>
-        {resourceConstraints.map((resourceConstraint: ResourceConstraintDetail) => {
+        {resourceConstraints.map((resourceConstraint: any) => {
+          // any -> ResourceConstraintDetail - update swagger
           const isCurrent = executionIdentifier === resourceConstraint.planExecutionId
+          const route = routes.toExecutionPipelineView({
+            orgIdentifier,
+            pipelineIdentifier: pipelineIdentifier,
+            executionIdentifier: resourceConstraint.planExecutionId || executionIdentifier,
+            projectIdentifier,
+            accountId,
+            module: module as Module,
+            source: 'executions'
+          })
           return (
             <div
               key={`${resourceConstraint.pipelineIdentifier}_${resourceConstraint.state}`}
               className={classnames(css.queuedExecutionsListItem, { [css.queuedExecutionsCurrentListItem]: isCurrent })}
             >
-              <div className={css.listItemName}>{resourceConstraint.pipelineIdentifier}</div>
-              <div className={css.listItemTime}>{moment(startTs).format('DD/MM/YYYY, h:mm:ss a')}</div>
+              <div className={css.listItemName}>
+                <NavLink
+                  to={route}
+                  onClick={e => {
+                    e.preventDefault()
+                    const baseUrl = window.location.href.split('#')[0]
+                    window.open(`${baseUrl}#${route}`)
+                  }}
+                >
+                  {resourceConstraint.pipelineName}
+                </NavLink>
+              </div>
+              <div className={css.listItemTime}>
+                {moment(resourceConstraint.startTs).format('DD/MM/YYYY, h:mm:ss a')}
+              </div>
               <div className={css.listItemState}>{renderState(getString, resourceConstraint.state, isCurrent)}</div>
             </div>
           )
@@ -74,11 +99,10 @@ const renderData = (
 
 export function QueuedExecutionsTab(props: ExecutionStepDetailsTabProps): React.ReactElement {
   const { getString } = useStrings()
-  const { executionIdentifier } = useParams<Record<string, string>>()
+  const params = useParams<Record<string, string>>()
 
   const { step } = props
   const resourceUnit = step?.stepParameters?.spec?.resourceUnit
-  const { accountId } = useParams<ExecutionPathProps>()
 
   const {
     data: resourceConstraintsData,
@@ -93,7 +117,7 @@ export function QueuedExecutionsTab(props: ExecutionStepDetailsTabProps): React.
       fetchResourceConstraints({
         queryParams: {
           resourceUnit,
-          accountId
+          accountId: params.accountId
         }
       })
     }
@@ -106,11 +130,7 @@ export function QueuedExecutionsTab(props: ExecutionStepDetailsTabProps): React.
       <div className={css.header}>
         <span className={css.headerLabel}>{getString('pipeline.queueStep.queuedByResourceKey')}</span> {resourceUnit}
       </div>
-      {
-        <section className={css.contentSection}>
-          {renderData(resourceConstraintsData, getString, executionIdentifier)}
-        </section>
-      }
+      {<section className={css.contentSection}>{renderData(resourceConstraintsData, getString, params)}</section>}
     </div>
   )
 }
