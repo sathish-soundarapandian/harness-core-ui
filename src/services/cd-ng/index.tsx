7e242ff4a931cd6dcadc446fb6876eb0ae6db173
Sagar Rajput <sagar.rajput@harness.io>
2022-06-28 00:22:19 +0530

    feat: [PIE-4272]: added modal steps for application setting

--- src/services/cd-ng/index.tsx
+++ src/services/cd-ng/index.tsx
@@ -10920,6 +10920,7 @@ export interface ServiceSpec {
   configFiles?: ConfigFileWrapper[]
   manifests?: ManifestConfigWrapper[]
   variables?: NGVariable[]
+  applicationSettings?: any
 }
 
 export interface ServiceUsageDTO {
