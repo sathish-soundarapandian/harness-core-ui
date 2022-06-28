3524d312ed4b13a3c44358ff58b1c5da113a1e53
Tamaraius <tamara.galovic@harness.io>
2022-06-28 19:07:34 +0200

    feat: initial setup 2

--- src/stringTypes.ts
+++ src/stringTypes.ts
@@ -3088,6 +3088,7 @@ export interface StringsMap {
   'pipeline.gitPullRequest': string
   'pipeline.gitPullRequestNumber': string
   'pipeline.gitRepo': string
+  'pipeline.harness': string
   'pipeline.imagePathLabel': string
   'pipeline.imageVersionDeprecated': string
   'pipeline.importSuccessMessage': string
@@ -3475,6 +3476,7 @@ export interface StringsMap {
   'pipeline.stagetoRetryFrom': string
   'pipeline.startTime': string
   'pipeline.startedOn': string
+  'pipeline.startupScript.file': string
   'pipeline.startupScript.fileDetails': string
   'pipeline.startupScript.fileSource': string
   'pipeline.startupScript.name': string
