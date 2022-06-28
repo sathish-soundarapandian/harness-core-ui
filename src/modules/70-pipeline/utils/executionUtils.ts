444da14800335e70855910191d207776a20cd820
PratyushGar <100121280+PratyushGar@users.noreply.github.com>
2022-06-28 11:27:20 +0530

    Fix: [PIE-4275]: step group parallel (#9694)
    * fix: added conditionalExecutionEnabled for stages in executionView
    
    * fix: support singleItem in processParallelData
    
    * fix: version bump

--- src/modules/70-pipeline/utils/executionUtils.ts
+++ src/modules/70-pipeline/utils/executionUtils.ts
@@ -36,6 +36,7 @@ import factory from '@pipeline/components/PipelineSteps/PipelineStepFactory'
 import { StepType } from '@pipeline/components/PipelineSteps/PipelineStepInterface'
 import { stagesCollection } from '@pipeline/components/PipelineStudio/Stages/StagesCollection'
 import { PipelineGraphState, PipelineGraphType } from '@pipeline/components/PipelineDiagram/types'
+import { getConditionalExecutionFlag } from '@pipeline/components/ExecutionStageDiagram/ExecutionStageDiagramUtils'
 import { isApprovalStep } from './stepUtils'
 import { StageType } from './stageHelpers'
 
@@ -1028,6 +1029,7 @@ export const processExecutionDataForGraph = (stages?: PipelineGraphState[]): Pip
           : currentStage?.type,
         data: {
           ...currentStage.data,
+          conditionalExecutionEnabled: getConditionalExecutionFlag(currentStage?.data?.nodeRunInfo),
           identifier: currentStageData?.nodeUuid || /* istanbul ignore next */ '',
           name: currentStageData?.name || currentStageData?.nodeIdentifier || /* istanbul ignore next */ '',
           status: currentStageData?.status as any,
@@ -1057,6 +1059,7 @@ export const processExecutionDataForGraph = (stages?: PipelineGraphState[]): Pip
             status: node?.status as never,
             data: {
               ...node,
+              conditionalExecutionEnabled: getConditionalExecutionFlag(node?.nodeRunInfo),
               identifier: node?.nodeUuid || /* istanbul ignore next */ '',
               name: node?.name || node?.nodeIdentifier || /* istanbul ignore next */ '',
               status: node?.status as never,
@@ -1086,6 +1089,7 @@ export const processExecutionDataForGraph = (stages?: PipelineGraphState[]): Pip
           : currentStage?.type,
         data: {
           ...stage,
+          conditionalExecutionEnabled: getConditionalExecutionFlag(stage?.nodeRunInfo),
           identifier: stage?.nodeUuid || /* istanbul ignore next */ '',
           name: stage?.name || stage?.nodeIdentifier || /* istanbul ignore next */ '',
           status: stage?.status as any,
