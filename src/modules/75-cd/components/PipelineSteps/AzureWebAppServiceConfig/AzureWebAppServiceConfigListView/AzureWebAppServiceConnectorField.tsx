fb75c5f440d53ef9c707b418244f03bbde0f5aef
Tamaraius <tamara.galovic@harness.io>
2022-06-30 14:41:59 +0200

    fix: initial setup 3

--- src/modules/75-cd/components/PipelineSteps/AzureWebAppServiceConfig/AzureWebAppServiceConfigListView/AzureWebAppServiceConnectorField.tsx
+++ src/modules/75-cd/components/PipelineSteps/AzureWebAppServiceConfig/AzureWebAppServiceConfigListView/AzureWebAppServiceConnectorField.tsx
@@ -18,7 +18,7 @@ function ConnectorField({ connectorRef, connectorName, connectorColor }: Connect
         <>
           <Text
             tooltip={
-              <Container className={css.borderRadius} padding="medium">
+              <Container padding="medium">
                 <div>
                   <Text font="small" color={Color.GREY_100}>
                     {connectorName}
