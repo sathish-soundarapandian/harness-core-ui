44a77f2080db8c6b4ac4af385f6d7ef14c039bbe
Tamaraius <tamara.galovic@harness.io>
2022-07-04 10:24:18 +0200

    feat: runtime view for startup script

---
+++ src/modules/75-cd/components/PipelineSteps/K8sServiceSpec/StartupScripts/GitStartupScript/GitStartupScriptRuntimeFields.tsx
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+import React, { useState } from 'react'
+import { FormInput, MultiTypeInputType } from '@harness/uicore'
+import { defaultTo, get } from 'lodash-es'
+import { FormMultiTypeConnectorField } from '@connectors/components/ConnectorReferenceField/FormMultiTypeConnectorField'
+import { useStrings } from 'framework/strings'
+import { useVariablesExpression } from '@pipeline/components/PipelineStudio/PiplineHooks/useVariablesExpression'
+import type { GitConfigDTO } from 'services/cd-ng'
+import { ManifestToConnectorMap } from '@pipeline/components/ManifestSelection/Manifesthelper'
+import type { Scope } from '@common/interfaces/SecretsInterface'
+import type { StartupScriptRenderProps } from '@cd/factory/StartupScriptFactory/StartupScriptBase'
+import { isFieldRuntime } from '../../K8sServiceSpecHelper'
+import { isFieldfromTriggerTabDisabled, shouldDisplayRepositoryName } from '../../ManifestSource/ManifestSourceUtils'
+import css from '../../KubernetesManifests/KubernetesManifests.module.scss'
+
+const GitStartupScriptRuntimeFields = ({
+  template,
+  initialValues,
+  path,
+  startupScriptPath,
+  startupScript,
+  fromTrigger,
+  allowableTypes,
+  accountId,
+  projectIdentifier,
+  orgIdentifier,
+  readonly,
+  repoIdentifier,
+  branch,
+  formik,
+  stageIdentifier
+}: StartupScriptRenderProps): React.ReactElement => {
+  const { getString } = useStrings()
+  const { expressions } = useVariablesExpression()
+  const [showRepoName, setShowRepoName] = useState(true)
+  const isFieldDisabled = (fieldName: string): boolean => {
+    // /* instanbul ignore else */
+    if (readonly) {
+      return true
+    }
+    return isFieldfromTriggerTabDisabled(fieldName, formik, stageIdentifier, '', fromTrigger)
+  }
+  return (
+    <>
+      {isFieldRuntime(`${startupScriptPath}.spec.store.spec.connectorRef`, template) && (
+        <div data-name="connectorRefContainer" className={css.verticalSpacingInput}>
+          <FormMultiTypeConnectorField
+            disabled={isFieldDisabled(`${startupScriptPath}.spec.store.spec.connectorRef`)}
+            name={`${path}.${startupScriptPath}.spec.store.spec.connectorRef`}
+            selected={get(initialValues, `${startupScriptPath}.spec.store.spec.connectorRef`, '')}
+            label={getString('connector')}
+            placeholder={''}
+            setRefValue
+            multiTypeProps={{
+              allowableTypes,
+              expressions
+            }}
+            width={391}
+            accountIdentifier={accountId}
+            projectIdentifier={projectIdentifier}
+            orgIdentifier={orgIdentifier}
+            type={ManifestToConnectorMap[defaultTo(startupScript?.spec.store.type, '')]}
+            onChange={(selected, _itemType, multiType) => {
+              const item = selected as unknown as { record?: GitConfigDTO; scope: Scope }
+              if (multiType === MultiTypeInputType.FIXED) {
+                if (shouldDisplayRepositoryName(item)) {
+                  setShowRepoName(false)
+                } else {
+                  setShowRepoName(true)
+                }
+              }
+            }}
+            gitScope={{
+              repo: defaultTo(repoIdentifier, ''),
+              branch: defaultTo(branch, ''),
+              getDefaultFromOtherRepo: true
+            }}
+          />
+        </div>
+      )}
+
+      {isFieldRuntime(`${startupScriptPath}.spec.store.spec.repoName`, template) && showRepoName && (
+        <div className={css.verticalSpacingInput}>
+          <FormInput.MultiTextInput
+            disabled={isFieldDisabled(`${startupScriptPath}.spec.store.spec.repoName`)}
+            name={`${path}.${startupScriptPath}.spec.store.spec.repoName`}
+            multiTextInputProps={{
+              expressions,
+              allowableTypes
+            }}
+            label={getString('common.repositoryName')}
+          />
+        </div>
+      )}
+
+      {isFieldRuntime(`${startupScriptPath}.spec.store.spec.branch`, template) && (
+        <div className={css.verticalSpacingInput}>
+          <FormInput.MultiTextInput
+            disabled={isFieldDisabled(`${startupScriptPath}.spec.store.spec.branch`)}
+            name={`${path}.${startupScriptPath}.spec.store.spec.branch`}
+            multiTextInputProps={{
+              expressions,
+              allowableTypes
+            }}
+            label={getString('pipelineSteps.deploy.inputSet.branch')}
+          />
+        </div>
+      )}
+      {isFieldRuntime(`${startupScriptPath}.spec.store.spec.commitId`, template) && (
+        <div className={css.verticalSpacingInput}>
+          <FormInput.MultiTextInput
+            disabled={isFieldDisabled(`${startupScriptPath}.spec.store.spec.commitId`)}
+            name={`${path}.${startupScriptPath}.spec.store.spec.commitId`}
+            multiTextInputProps={{
+              expressions,
+              allowableTypes
+            }}
+            label={getString('pipelineSteps.commitIdValue')}
+          />
+        </div>
+      )}
+    </>
+  )
+}
+export default GitStartupScriptRuntimeFields
