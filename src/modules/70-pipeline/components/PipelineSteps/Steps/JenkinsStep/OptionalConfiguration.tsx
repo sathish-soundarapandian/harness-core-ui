95643f80ca4230a35e4d3ad666f0b3937349d8b4
satyam srivastava <satyam.srivastava@harness.io>
2022-07-25 20:37:09 +0530

    fix: removed environment variable checkbox

--- src/modules/70-pipeline/components/PipelineSteps/Steps/JenkinsStep/OptionalConfiguration.tsx
+++ src/modules/70-pipeline/components/PipelineSteps/Steps/JenkinsStep/OptionalConfiguration.tsx
@@ -15,15 +15,13 @@ function OptionalConfiguration(props: { readonly?: boolean }): React.ReactElemen
   const { readonly } = props
   const { getString } = useStrings()
   return (
-    <>
-      <div className={cx(stepCss.formGroup)}>
-        <FormInput.CheckBox
-          name="spec.unstableStatusAsSuccess"
-          label={getString('pipeline.jenkinsStep.unstableStatusAsSuccess')}
-          disabled={readonly}
-        />
-      </div>
-    </>
+    <div className={cx(stepCss.formGroup)}>
+      <FormInput.CheckBox
+        name="spec.unstableStatusAsSuccess"
+        label={getString('pipeline.jenkinsStep.unstableStatusAsSuccess')}
+        disabled={readonly}
+      />
+    </div>
   )
 }
 
