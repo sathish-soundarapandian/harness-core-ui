cb343c537f2720b155b3bb8aa2d914e31aedbc77
Radu Jomir <radu.jomir@harness.io>
2022-07-29 15:56:00 +0300

    feat: [CDS-40604]: PDC connector improvements

--- src/stringTypes.ts
+++ src/stringTypes.ts
@@ -2234,10 +2234,16 @@ export interface StringsMap {
   'connectors.pdc.editHosts': string
   'connectors.pdc.errorUploading': string
   'connectors.pdc.hosts': string
+  'connectors.pdc.hostsJson': string
   'connectors.pdc.hostsRule': string
   'connectors.pdc.hostsUpload1': string
   'connectors.pdc.hostsUpload2': string
+  'connectors.pdc.invalidJsonError': string
+  'connectors.pdc.manuallyHosts': string
+  'connectors.pdc.replaceJsonFile': string
+  'connectors.pdc.selectLabel': string
   'connectors.pdc.testConnection.step1': string
+  'connectors.pdc.uploadJson': string
   'connectors.placeholder.harnessImageConnectorRef': string
   'connectors.policyEvaluations.failedToSave': string
   'connectors.policyEvaluations.warning': string
