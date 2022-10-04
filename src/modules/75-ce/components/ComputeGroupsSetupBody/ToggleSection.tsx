55b25f6aef12641dc7dbdf186127fa98e531261c
Rishabh Kumar <rishabh.kumar@harness.io>
2022-10-04 14:56:12 +0530

    feat: styling for toggle section

--- src/modules/75-ce/components/ComputeGroupsSetupBody/ToggleSection.tsx
+++ src/modules/75-ce/components/ComputeGroupsSetupBody/ToggleSection.tsx
@@ -6,33 +6,42 @@
  */
 
 import React from 'react'
+import cx from 'classnames'
 import { Collapse } from '@blueprintjs/core'
 import { Color, Container, FlexExpander, FontVariation, Icon, Layout, Text, Toggle, useToggle } from '@harness/uicore'
+import css from './ComputeGroupsSetupBody.module.scss'
 
 interface ToggleSectionProps {
   title: string
   subTitle: string
+  className?: string
 }
 
-const ToggleSection: React.FC<ToggleSectionProps> = ({ children, title, subTitle }) => {
+const ToggleSection: React.FC<ToggleSectionProps> = ({ children, title, subTitle, className }) => {
   const [isOpen, toggleIsOpen] = useToggle(false)
   const [isEnabled, toggleIsEnabled] = useToggle(false)
   return (
-    <Layout.Vertical>
-      <Layout.Horizontal onClick={toggleIsOpen}>
+    <Layout.Vertical className={cx(css.toggleSectionContainer, className)}>
+      <Layout.Horizontal onClick={toggleIsOpen} className={css.toggleHeader}>
         <Container>
           <Toggle checked={isEnabled} onChange={toggleIsEnabled} />
         </Container>
-        <Layout.Vertical>
-          <Text font={{ variation: FontVariation.H3 }}>{title}</Text>
-          <Text>{subTitle}</Text>
+        <Layout.Vertical spacing={'small'}>
+          <Text font={{ variation: FontVariation.H4 }} color={Color.GREY_800}>
+            {title}
+          </Text>
+          <Text font={{ variation: FontVariation.SMALL }} color={Color.GREY_600}>
+            {subTitle}
+          </Text>
         </Layout.Vertical>
         <FlexExpander />
         <Layout.Horizontal flex>
-          <Icon name="caret-down" color={Color.PRIMARY_7} />
+          <Icon name="main-chevron-down" color={Color.PRIMARY_7} />
         </Layout.Horizontal>
       </Layout.Horizontal>
-      <Collapse isOpen={isOpen}>{children}</Collapse>
+      <Collapse isOpen={isOpen} className={isOpen ? css.collapsibleBody : undefined}>
+        {children}
+      </Collapse>
     </Layout.Vertical>
   )
 }
