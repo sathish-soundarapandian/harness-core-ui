35fbad8c68e18a9f4c858a4d5a86c7bd3b32697e
Yogesh Chaudhary <yogesh.chaudhary@harness.io>
2022-10-07 14:44:21 +0530

    feat: [CDS-42781]: template variables support

--- src/modules/70-pipeline/utils/__tests__/templateUtils.test.tsx
+++ src/modules/70-pipeline/utils/__tests__/templateUtils.test.tsx
@@ -6,9 +6,7 @@
  */
 
 import React from 'react'
-import * as uuid from 'uuid'
 import { render } from '@testing-library/react'
-import type { TemplateSummaryResponse } from 'services/template-ng'
 import { TestWrapper } from '@common/utils/testUtils'
 import * as utils from '../templateUtils'
 import { replaceDefaultValues } from '../templateUtils'
@@ -47,52 +45,6 @@ jest.mock('services/template-ng', () => {
 })
 
 describe('templateUtils', () => {
-  describe('createStepNodeFromTemplate tests', () => {
-    const template: TemplateSummaryResponse = {
-      accountId: 'px7xd_BFRCi-pfWPYXVjvw',
-      childType: 'Http',
-      description: '',
-      identifier: 'Test_Http_Step',
-      lastUpdatedAt: 1641755022840,
-      name: 'Test Http Step',
-      orgIdentifier: 'default',
-      projectIdentifier: 'Yogesh_Test',
-      stableTemplate: false,
-      tags: {},
-      templateEntityType: 'Step',
-      templateScope: 'project',
-      version: 0,
-      versionLabel: 'v2',
-      yaml: 'template:\n    name: Test Http Step\n    identifier: Test_Http_Step\n    versionLabel: v2\n    type: Step\n    projectIdentifier: Yogesh_Test\n    orgIdentifier: default\n    tags: {}\n    spec:\n        type: Http\n        timeout: 30s\n        spec:\n            url: <+input>\n            method: GET\n            headers: []\n            outputVariables: []\n            requestBody: <+input>\n'
-    }
-    test('when isCopied is false', () => {
-      jest.spyOn(uuid, 'v5').mockReturnValue('MockedUUID')
-      const result = utils.createStepNodeFromTemplate(template)
-      expect(result).toEqual({
-        name: 'Test Http Step',
-        identifier: 'MockedUUID',
-        template: { templateRef: 'Test_Http_Step', versionLabel: 'v2' }
-      })
-    })
-    test('when isCopied is true', () => {
-      jest.spyOn(uuid, 'v5').mockReturnValue('MockedUUID')
-      const result = utils.createStepNodeFromTemplate(template, true)
-      expect(result).toEqual({
-        name: 'Test Http Step',
-        identifier: 'MockedUUID',
-        spec: {
-          headers: [],
-          method: 'GET',
-          outputVariables: [],
-          requestBody: '<+input>',
-          url: '<+input>'
-        },
-        timeout: '30s',
-        type: 'Http'
-      })
-    })
-  })
-
   test('replaceDefaultValues test', () => {
     const template = {
       var1: '<+input>',
