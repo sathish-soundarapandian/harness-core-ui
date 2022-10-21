18680abf3fae6a0471607642f11e2dbde2dc1708
dhruv upadhyay <dhruv.upadhyay@harness.io>
2022-10-21 22:15:34 +0530

    feat: [PLG-1606]: ff cleanup test cases

--- src/modules/10-common/layouts/__tests__/DefaultLayout.test.tsx
+++ src/modules/10-common/layouts/__tests__/DefaultLayout.test.tsx
@@ -66,7 +66,7 @@ const useGetUsageAndLimitReturnMock = {
 jest.mock('@common/hooks/useFeatureFlag', () => ({
   useFeatureFlag: jest.fn(() => true),
   useFeatureFlags: jest.fn(() => {
-    return { FEATURE_ENFORCEMENT_ENABLED: true }
+    return { FEATURE_ENFORCEMENT_ENABLED: true, FREE_PLAN_ENFORCEMENT_ENABLED: false }
   })
 }))
 
