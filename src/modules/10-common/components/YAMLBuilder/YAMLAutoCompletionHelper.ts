dad0ea84317b5884bae7543c44bbce414f3c04b3
Vardan Bansal <vardan.bansal@harness.io>
2022-10-26 22:52:24 +0530

    fix: [CI-5855]: remove version from schema and yaml

--- src/modules/10-common/components/YAMLBuilder/YAMLAutoCompletionHelper.ts
+++ src/modules/10-common/components/YAMLBuilder/YAMLAutoCompletionHelper.ts
@@ -10,14 +10,14 @@ export const AutoCompletionMap = new Map<string, { autoCompletionYAML: string }>
     'name: Java with Maven',
     {
       autoCompletionYAML:
-        '\nversion: 1\nstages:\n  - name: Build and test Java app\n    type: ci\n    steps:\n      - name: Build\n        type: script\n        spec:\n            run: |-\n                echo "Hello world"\n                mvn install -DskipTests\n      - name: Run Tests\n        type: run_tests\n        spec:\n            language: Java\n            build_tool: Maven\n            build_args: test\n            test_reports:\n              type: JUnit\n              paths:\n                - \'**/*.xml\'\n            strategy:\n              parallelism: 3'
+        '\nstages:\n  - name: Build and test Java app\n    type: ci\n    steps:\n      - name: Build\n        type: script\n        spec:\n            run: |-\n                echo "Hello world"\n                mvn install -DskipTests\n      - name: Run Tests\n        type: run_tests\n        spec:\n            language: Java\n            build_tool: Maven\n            build_args: test\n            test_reports:\n              type: JUnit\n              paths:\n                - \'**/*.xml\'\n            strategy:\n              parallelism: 3'
     }
   ],
   [
     'name: Java with Gradle',
     {
       autoCompletionYAML:
-        '\nversion: 1\nstages:\n  - name: Build and test Java app\n    type: ci\n    steps:\n      - name: Build\n        type: script\n        spec:\n            run: |-\n                echo "Hello world"\n                mvn install -DskipTests\n      - name: Run Tests\n        type: run_tests\n        spec:\n            language: Java\n            build_tool: Maven\n            build_args: test\n            test_reports:\n              type: JUnit\n              paths:\n                - \'**/*.xml\'\n            strategy:\n              parallelism: 3'
+        '\nstages:\n  - name: Build and test Java app\n    type: ci\n    steps:\n      - name: Build\n        type: script\n        spec:\n            run: |-\n              echo "Welcome to Harness CI"\n              gradle build -x test\n      - name: Run Tests\n        type: run_tests\n        spec:\n            language: Java\n            build_tool: Gradle\n            build_args: test     \n            test_reports:\n                type: JUnit \n                paths: \n                  - "**/*.xml"    \n            strategy:\n               parallelism: 3'
     }
   ]
 ])
