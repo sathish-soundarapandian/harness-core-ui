e7b21fd55ea55bfcd1d750a197370072db588734
Avinash Madhwani <avinash.madhwani@harness.io>
2022-11-16 22:56:14 +0530

    fix: account overview page changes

---
+++ src/modules/45-projects-orgs/pages/OverviewDashboardPage/ResourcesCard/ResourcesCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import css from './ResourcesCard.module.scss'
+
+export const ResourcesCard = () => {
+  return <div className={css.container}>Resources Card</div>
+}
+
+export default ResourcesCard
