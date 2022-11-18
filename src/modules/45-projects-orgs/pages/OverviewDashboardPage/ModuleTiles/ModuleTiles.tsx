4ef583ac7ce070fddfca3c0b933a5df72598d289
Abhinav Rastogi <abhinav.rastogi@harness.io>
2022-11-18 16:34:19 +0530

    feat: expanding module tiles

--- src/modules/45-projects-orgs/pages/OverviewDashboardPage/ModuleTiles/ModuleTiles.tsx
+++ src/modules/45-projects-orgs/pages/OverviewDashboardPage/ModuleTiles/ModuleTiles.tsx
@@ -1,7 +1,44 @@
 import React from 'react'
+import cx from 'classnames'
+import { Card, Container } from '@harness/uicore'
+import type { Module as ModuleName } from 'framework/types/ModuleName'
+import css from './ModuleTiles.module.scss'
 
-const ModuleTiles = () => {
-  return <div>Module tiles</div>
+export default function ModuleTiles(): React.ReactElement {
+  const modules: ModuleName[] = ['cd', 'ci', 'cf', 'sto', 'cv', 'ce', 'chaos']
+  const moduleToCardMap: Record<ModuleName, React.ReactElement> = {
+    cd: <div>cd</div>,
+    ci: <div>ci</div>,
+    cf: <div>ff</div>,
+    sto: <div>sto</div>,
+    cv: <div>srm</div>,
+    ce: <div>ccm</div>,
+    chaos: <div>chaos</div>,
+    scm: <div>scm</div>
+  }
+  const [selectedModule, setSelectedModule] = React.useState<ModuleName>()
+  const numOfColumns = 3
+
+  return (
+    <Container className={css.moduleTiles}>
+      {modules.map((harnessModule, index) => {
+        const isExpanded = harnessModule === selectedModule
+        const expandOnRow = Math.floor(index / numOfColumns) + 1
+        const expandOnColumn = index % 3 < numOfColumns - 1 ? (index % 3) + 1 : index % 3
+
+        return (
+          <Card
+            className={cx(css.tile, { [css.expanded]: isExpanded })}
+            key={harnessModule}
+            onClick={() => {
+              setSelectedModule(isExpanded ? undefined : harnessModule)
+            }}
+            style={isExpanded ? { gridRowStart: expandOnRow, gridColumnStart: expandOnColumn } : {}}
+          >
+            {moduleToCardMap[harnessModule]}
+          </Card>
+        )
+      })}
+    </Container>
+  )
 }
-
-export default ModuleTiles
