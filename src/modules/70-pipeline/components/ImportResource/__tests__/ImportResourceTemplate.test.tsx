7dcdd5643d3945ec6a289c97d0f6026efb5ddc3c
sumanmabd <guguloth.suman@harness.io>
2022-11-21 03:00:44 +0530

    fix: [PIE-6485]: jest for import template

--- src/modules/70-pipeline/components/ImportResource/__tests__/ImportResourceTemplate.test.tsx
+++ src/modules/70-pipeline/components/ImportResource/__tests__/ImportResourceTemplate.test.tsx
@@ -1,7 +1,15 @@
-import { render } from '@testing-library/react'
+import { act, render, waitFor } from '@testing-library/react'
 import React from 'react'
+import userEvent from '@testing-library/user-event'
 import routes from '@common/RouteDefinitions'
-import { accountPathProps, modulePathProps, orgPathProps, templatePathProps } from '@common/utils/routeUtils'
+import {
+  accountPathProps,
+  modulePathProps,
+  orgPathProps,
+  projectPathProps,
+  templatePathProps
+} from '@common/utils/routeUtils'
+import * as templateNg from 'services/template-ng'
 import { TestWrapper } from '@common/utils/testUtils'
 import { mockRepos, mockBranches, gitConnectorMock } from '@gitsync/components/GitSyncForm/__tests__/mockdata'
 import { ResourceType } from '@common/interfaces/GitSyncInterface'
@@ -32,7 +40,8 @@ const TEST_TEMPLATES_PATH = routes.toTemplates({
   ...accountPathProps,
   ...orgPathProps,
   ...templatePathProps,
-  ...modulePathProps
+  ...modulePathProps,
+  ...projectPathProps
 })
 const TEST_PATH_PARAMS = {
   accountId: 'testAcc',
@@ -45,15 +54,16 @@ const onSuccess = jest.fn()
 const onFailure = jest.fn()
 const onCancelClick = jest.fn()
 
-// const initialValues = {
-//   identifier: 'Github_Template',
-//   name: 'Github Template',
-//   description: 'importing Template',
-//   connectorRef: 'testConnectorRef',
-//   repoName: 'testRepo',
-//   branch: 'testBranch',
-//   filePath: '.harness/Github_Template.yaml'
-// }
+const initialValues = {
+  identifier: 'Github_Template',
+  name: 'Github Template',
+  description: 'importing Template',
+  connectorRef: 'testConnectorRef',
+  repoName: 'testRepo',
+  branch: 'testBranch',
+  filePath: '.harness/Github_Template.yaml',
+  versionLabel: 'Version1'
+}
 
 describe('ImportResource - Template', () => {
   beforeEach(() => {
@@ -70,5 +80,104 @@ describe('ImportResource - Template', () => {
     )
     expect(getByText('name')).toBeInTheDocument()
     expect(container).toMatchSnapshot()
+  })
+
+  test('clicking on cancel button should call onCancelClick prop function', () => {
+    const { getByText } = render(
+      <TestWrapper path={TEST_TEMPLATES_PATH} pathParams={TEST_PATH_PARAMS}>
+        <ImportResource resourceType={ResourceType.TEMPLATE} onCancelClick={onCancelClick} />
+      </TestWrapper>
+    )
+
+    const cancelButton = getByText('cancel')
+    userEvent.click(cancelButton)
+    expect(onCancelClick).toHaveBeenCalled()
+    expect(onCancelClick).toHaveBeenCalledTimes(1)
+  })
+
+  test('when onCancelClick prop is not passed - clicking on cancel button should not call onCancelClick prop function', () => {
+    const { getByText } = render(
+      <TestWrapper path={TEST_TEMPLATES_PATH} pathParams={TEST_PATH_PARAMS}>
+        <ImportResource resourceType={ResourceType.TEMPLATE} />
+      </TestWrapper>
+    )
+
+    const cancelButton = getByText('cancel')
+    userEvent.click(cancelButton)
+    expect(onCancelClick).not.toHaveBeenCalled()
+    expect(onCancelClick).toHaveBeenCalledTimes(0)
+  })
+
+  test('provide required values and click on import button', async () => {
+    const { container, getByText } = render(
+      <TestWrapper path={TEST_TEMPLATES_PATH} pathParams={TEST_PATH_PARAMS}>
+        <ImportResource
+          resourceType={ResourceType.TEMPLATE}
+          onCancelClick={onCancelClick}
+          onSuccess={onSuccess}
+          initialValues={initialValues}
+        />
+      </TestWrapper>
+    )
+
+    const importButton = getByText('common.import')
+    expect(container).toMatchSnapshot()
+    act(() => {
+      userEvent.click(importButton)
+    })
+    await waitFor(() => expect(getByText('pipeline.importSuccessMessage')).toBeDefined())
+    await waitFor(() => expect(onSuccess).toHaveBeenCalled())
+    expect(onSuccess).toHaveBeenCalledTimes(1)
+  })
+  test('when onSuccess prop is not passed - provide required values and click on import button', async () => {
+    const { container, getByText } = render(
+      <TestWrapper path={TEST_TEMPLATES_PATH} pathParams={TEST_PATH_PARAMS}>
+        <ImportResource
+          resourceType={ResourceType.TEMPLATE}
+          onCancelClick={onCancelClick}
+          initialValues={initialValues}
+        />
+      </TestWrapper>
+    )
+
+    const importButton = getByText('common.import')
+    expect(container).toMatchSnapshot()
+    act(() => {
+      userEvent.click(importButton)
+    })
+    await waitFor(() => expect(getByText('pipeline.importSuccessMessage')).toBeDefined())
+    await waitFor(() => expect(onSuccess).not.toHaveBeenCalled())
+    expect(onSuccess).toHaveBeenCalledTimes(0)
+  })
+
+  test('when import throws error WITHOUT responseMessages', async () => {
+    jest.spyOn(templateNg, 'importTemplatePromise').mockImplementation((): any => {
+      return Promise.reject({
+        status: 'ERROR',
+        code: 'INVALID_REQUEST',
+        message: 'Invalid Request: Error while importing template'
+      })
+    })
+
+    const { container, getByText } = render(
+      <TestWrapper path={TEST_TEMPLATES_PATH} pathParams={TEST_PATH_PARAMS}>
+        <ImportResource
+          resourceType={ResourceType.TEMPLATE}
+          onCancelClick={onCancelClick}
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          initialValues={initialValues}
+        />
+      </TestWrapper>
+    )
+
+    const importButton = getByText('common.import')
+    expect(container).toMatchSnapshot()
+    act(() => {
+      userEvent.click(importButton)
+    })
+    await waitFor(() => expect(getByText('Invalid Request: Error while importing template')).toBeDefined())
+    await waitFor(() => expect(onFailure).toHaveBeenCalled())
+    expect(onFailure).toHaveBeenCalledTimes(1)
   })
 })
