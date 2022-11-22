3d716721c43e247bb11a43a0a29b49fd1136d04b
dhruv upadhyay <dhruv.upadhyay@harness.io>
2022-11-22 16:27:12 +0530

    feat: [PLG-1565]: service license table changes

--- src/modules/33-auth-settings/pages/subscriptions/overview/SubscriptionOverview.tsx
+++ src/modules/33-auth-settings/pages/subscriptions/overview/SubscriptionOverview.tsx
@@ -5,9 +5,9 @@
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
 
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
-import { Layout } from '@harness/uicore'
+import { Layout, shouldShowError } from '@harness/uicore'
 import type { ModuleName } from 'framework/types/ModuleName'
 
 import type { ModuleLicenseDTO } from 'services/cd-ng'
@@ -15,8 +15,17 @@ import { useFeatureFlag } from '@common/hooks/useFeatureFlag'
 import { FeatureFlag } from '@common/featureFlags'
 import SubscriptionDetailsCard from './SubscriptionDetailsCard'
 import SubscriptionUsageCard from './SubscriptionUsageCard'
-import type { TrialInformation } from '../SubscriptionsPage'
+import { ServiceLicenseTable } from './ServiceLicenseTable'
 
+import type { TrialInformation } from '../SubscriptionsPage'
+import type { PipelineListPageQueryParams, PipelineListPagePathParams } from '@pipeline/pages/pipeline-list/types'
+import { useUpdateQueryParams, useQueryParams } from '@common/hooks'
+import { PagePMSPipelineSummaryResponse, PipelineFilterProperties, useGetPipelineList } from 'services/pipeline-ng'
+import { usePreferenceStore, PreferenceScope } from 'framework/PreferenceStore/PreferenceStoreContext'
+import { queryParamOptions, ProcessedPipelineListPageQueryParams } from '@pipeline/pages/pipeline-list/PipelineListPage'
+import { showError } from '@cf/pages/pipeline-studio/views/StageOverview/__tests__/StageOverviewTestHelper'
+import useRBACError from '@rbac/utils/useRBACError/useRBACError'
+import { useParams } from 'react-router'
 interface SubscriptionOverviewProps {
   accountName?: string
   licenseData?: ModuleLicenseDTO
@@ -24,11 +33,75 @@ interface SubscriptionOverviewProps {
   trialInformation: TrialInformation
   refetchGetLicense?: () => void
 }
+const {
+  mutate: loadPipelineList,
+  error: pipelineListLoadingError,
+  loading: isPipelineListLoading
+} = useGetPipelineList({
+  queryParamStringifyOptions: { arrayFormat: 'comma' }
+})
 
 const SubscriptionOverview: React.FC<SubscriptionOverviewProps> = props => {
   const { accountName, licenseData, module, trialInformation, refetchGetLicense } = props
   const enabled = useFeatureFlag(FeatureFlag.VIEW_USAGE_ENABLED)
+  const [pipelineList, setPipelineList] = useState<PagePMSPipelineSummaryResponse | undefined>()
+  const { updateQueryParams } = useUpdateQueryParams<Partial<PipelineListPageQueryParams>>()
+  const { preference: sortingPreference, setPreference: setSortingPreference } = usePreferenceStore<string | undefined>(
+    PreferenceScope.USER,
+    'PipelineSortingPreference'
+  )
+  const pathParams = useParams<PipelineListPagePathParams>()
+  const { projectIdentifier, orgIdentifier, accountId } = pathParams
+  const { getRBACErrorMessage } = useRBACError()
+  const queryParams = useQueryParams<ProcessedPipelineListPageQueryParams>(queryParamOptions)
+  const sort = sortingPreference ? JSON.parse(sortingPreference) : queryParams.sort
+  const { searchTerm, repoIdentifier, branch, page, size, repoName } = queryParams
+  const fetchPipelines = useCallback(async () => {
+    try {
+      const filter: PipelineFilterProperties = {
+        filterType: 'PipelineSetup',
 
+        repoName
+      }
+      const { status, data } = await loadPipelineList(filter, {
+        queryParams: {
+          accountIdentifier: accountId,
+          projectIdentifier,
+          orgIdentifier,
+          searchTerm,
+          page,
+          sort,
+          size,
+          ...(repoIdentifier &&
+            branch && {
+              repoIdentifier,
+              branch
+            })
+        }
+      })
+      if (status === 'SUCCESS') {
+        setPipelineList(data)
+      }
+    } catch (e) {
+      if (shouldShowError(e)) {
+        showError(getRBACErrorMessage(e), undefined, 'pipeline.fetch.pipeline.error')
+      }
+    }
+  }, [
+    accountId,
+    branch,
+    orgIdentifier,
+    page,
+    projectIdentifier,
+    repoIdentifier,
+    searchTerm,
+    size,
+    sort?.toString(),
+    repoName
+  ])
+  useEffect(() => {
+    fetchPipelines()
+  }, [fetchPipelines])
   return (
     <Layout.Vertical spacing="large" width={'90%'}>
       <SubscriptionDetailsCard
@@ -39,6 +112,15 @@ const SubscriptionOverview: React.FC<SubscriptionOverviewProps> = props => {
         refetchGetLicense={refetchGetLicense}
       />
       {enabled && licenseData && <SubscriptionUsageCard module={module} licenseData={licenseData} />}
+      <ServiceLicenseTable
+        gotoPage={pageNumber => updateQueryParams({ page: pageNumber })}
+        data={pipelineList}
+        setSortBy={sortArray => {
+          setSortingPreference(JSON.stringify(sortArray))
+          updateQueryParams({ sort: sortArray })
+        }}
+        sortBy={sort}
+      />
     </Layout.Vertical>
   )
 }
