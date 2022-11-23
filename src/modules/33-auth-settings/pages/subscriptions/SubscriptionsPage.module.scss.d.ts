329050ec20d7a81d49ee2d148d08d95574d6b891
dhruv upadhyay <dhruv.upadhyay@harness.io>
2022-11-23 12:33:45 +0530

    feat: [PLG-1565]: cahnges for license table

--- src/modules/33-auth-settings/pages/subscriptions/SubscriptionsPage.module.scss.d.ts
+++ src/modules/33-auth-settings/pages/subscriptions/SubscriptionsPage.module.scss.d.ts
@@ -7,6 +7,10 @@
  **/
 // this is an auto-generated file, do not update this manually
 declare const styles: {
+  readonly activeServiceLink: string
+  readonly badge: string
+  readonly badgeText: string
+  readonly badgesContainer: string
   readonly banner: string
   readonly bannerIcon: string
   readonly cd: string
@@ -16,10 +20,12 @@ declare const styles: {
   readonly cv: string
   readonly expired: string
   readonly expiryCountdown: string
+  readonly failed24HrsExecutionsBadge: string
   readonly moduleIcons: string
   readonly moduleSelectCard: string
   readonly moduleSelectCards: string
   readonly outterCard: string
+  readonly runningExecutions: string
   readonly selected: string
   readonly sto: string
   readonly subscriptionTabButton: string
