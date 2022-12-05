3ed9225338db9787e19716bd6109de76b40d655b
Avinash Madhwani <avinash.madhwani@harness.io>
2022-12-05 13:45:17 +0530

    fix: added test cases

--- src/modules/45-projects-orgs/pages/LandingDashboardPageV2/OverviewGlanceCardsContainer/OverviewGlanceCardV2/OverviewGlanceCardV2.tsx
+++ src/modules/45-projects-orgs/pages/LandingDashboardPageV2/OverviewGlanceCardsContainer/OverviewGlanceCardV2/OverviewGlanceCardV2.tsx
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
 import React from 'react'
 import { Container, Icon, Layout, Text } from '@harness/uicore'
 import { FontVariation, Color } from '@harness/design-system'
@@ -11,7 +18,6 @@ import css from './OverviewGlanceCardV2.module.scss'
 
 export interface OverviewGlanceCardV2Props {
   count?: number
-  delta?: JSX.Element
   label: keyof StringsMap
   loading: boolean
   className?: string
@@ -65,6 +71,7 @@ const OverviewGlanceCardV2: React.FC<OverviewGlanceCardV2Props> = props => {
 
   return (
     <Layout.Vertical
+      data-testid="overviewcard"
       className={cx(css.container, { [css.active]: !!redirectUrl }, className)}
       onClick={() => {
         if (redirectUrl) {
