787f691ced63e6a2e94f6dabf5ba86b3d4ee85ab
Avinash Madhwani <avinash.madhwani@harness.io>
2022-12-06 14:26:34 +0530

    fix: added test cases

--- src/modules/45-projects-orgs/pages/LandingDashboardPageV2/PreferencesCard/__tests__/PreferenceCard.test.tsx
+++ src/modules/45-projects-orgs/pages/LandingDashboardPageV2/PreferencesCard/__tests__/PreferenceCard.test.tsx
@@ -1,16 +1,42 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { TestWrapper } from '@common/utils/testUtils'
+import { usePreferenceStore } from 'framework/PreferenceStore/PreferenceStoreContext'
+import type { SavedProjectDetails } from 'framework/AppStore/AppStoreContext'
 import PreferencesCard from '../PreferencesCard'
 
+jest.mock('framework/PreferenceStore/PreferenceStoreContext')
+
 describe('Preference card test', () => {
-  test('render', () => {
-    const { container } = render(
+  test('click on favorites', () => {
+    const { container, queryByText } = render(
       <TestWrapper>
         <PreferencesCard />
       </TestWrapper>
     )
-    screen.debug(container)
-    expect(container).toBeDefined()
+    const favortiesTab = container.querySelector('[data-tab-id="favorites"]')
+    fireEvent.click(favortiesTab!)
+
+    expect(queryByText('common.comingSoon')).not.toBeNull()
+  })
+
+  test('click on favorites', () => {
+    ;(usePreferenceStore as jest.Mock).mockImplementation(() => {
+      return {
+        preference: [
+          { projectIdentifier: 'dummyProjectIdentifier', orgIdentifier: 'dummyOrgIdentifier', name: 'projectName' }
+        ] as SavedProjectDetails[],
+        clearPreference: jest.fn
+      }
+    })
+    const { container, queryByText } = render(
+      <TestWrapper>
+        <PreferencesCard />
+      </TestWrapper>
+    )
+    const favortiesTab = container.querySelector('[data-tab-id="favorites"]')
+    fireEvent.click(favortiesTab!)
+
+    expect(queryByText('common.comingSoon')).not.toBeNull()
   })
 })
