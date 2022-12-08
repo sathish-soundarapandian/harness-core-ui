261ff9fb3634a962a3cac3386596e66b50bd7220
sumathippeswamy <suma.thippeswamy@harness.io>
2022-12-08 22:57:41 +0530

    renamed file

--- scripts/ci/release-branch-cut.sh
+++ scripts/ci/release-branch-cut.sh
@@ -26,7 +26,7 @@ chmod 700 scripts/ci/*.*
 #  scripts/ci/release-branch-create-versions.sh
 #fi
 # updating the jira tickets
-scripts/ci/jira-tagging-NGUI-QA.sh
+scripts/ci/jira-tagging-ngui-qa.sh
 
 # commit to develop
 git commit -nm "Branching to $NEW_BRANCH"
