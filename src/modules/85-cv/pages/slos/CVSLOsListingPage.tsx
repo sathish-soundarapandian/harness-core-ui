07f30cf4a3532fbbc8ae9841e7cc0b92ee6e3b56
deepesh-ui <deepesh.kumar@harness.io>
2022-12-10 13:45:43 +0530

    feat: [SRM-13213]: fixed failing cypress

--- src/modules/85-cv/pages/slos/CVSLOsListingPage.tsx
+++ src/modules/85-cv/pages/slos/CVSLOsListingPage.tsx
@@ -277,15 +277,15 @@ const CVSLOsListingPage: React.FC<CVSLOsListingPageProps> = ({ monitoredService 
     const slo = row?.original
     const { name = '', sloIdentifier = '', description = '', sloType = '' } = slo || {}
     const path = isAccountLevel
-      ? routes.toCVSLODetailsPage({
+      ? routes.toAccountCVSLODetailsPage({
+          identifier: sloIdentifier,
+          accountId
+        })
+      : routes.toCVSLODetailsPage({
           identifier: sloIdentifier,
           accountId,
           orgIdentifier,
           projectIdentifier
-        })
-      : routes.toAccountCVSLODetailsPage({
-          identifier: sloIdentifier,
-          accountId
         })
     const queryParams = getSearchString({ sloType })
     return (
