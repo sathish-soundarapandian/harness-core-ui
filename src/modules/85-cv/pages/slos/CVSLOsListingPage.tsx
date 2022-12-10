3db8240de791c61f560bb56e696ce7cdafd71303
deepesh-ui <deepesh.kumar@harness.io>
2022-12-10 10:54:13 +0530

    feat: [SRM-13213]: fixed PR comments on PR 12917

--- src/modules/85-cv/pages/slos/CVSLOsListingPage.tsx
+++ src/modules/85-cv/pages/slos/CVSLOsListingPage.tsx
@@ -276,18 +276,17 @@ const CVSLOsListingPage: React.FC<CVSLOsListingPageProps> = ({ monitoredService 
   const RenderSLOName: Renderer<CellProps<any>> = ({ row }) => {
     const slo = row?.original
     const { name = '', sloIdentifier = '', description = '', sloType = '' } = slo || {}
-    const path =
-      orgIdentifier && projectIdentifier
-        ? routes.toCVSLODetailsPage({
-            identifier: sloIdentifier,
-            accountId,
-            orgIdentifier,
-            projectIdentifier
-          })
-        : routes.toAccountCVSLODetailsPage({
-            identifier: sloIdentifier,
-            accountId
-          })
+    const path = isAccountLevel
+      ? routes.toCVSLODetailsPage({
+          identifier: sloIdentifier,
+          accountId,
+          orgIdentifier,
+          projectIdentifier
+        })
+      : routes.toAccountCVSLODetailsPage({
+          identifier: sloIdentifier,
+          accountId
+        })
     const queryParams = getSearchString({ sloType })
     return (
       <Link to={`${path}${queryParams}`}>
