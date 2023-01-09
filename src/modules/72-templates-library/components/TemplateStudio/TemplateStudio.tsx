35c9e9d088de8d4a7b3d958af22bd07afa9c1e6a
Naman Gupta <naman.gupta@harness.io>
2023-01-09 09:32:37 +0530

    pipeline-studio-banner

--- src/modules/72-templates-library/components/TemplateStudio/TemplateStudio.tsx
+++ src/modules/72-templates-library/components/TemplateStudio/TemplateStudio.tsx
@@ -5,8 +5,11 @@
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
 
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Callout } from '@blueprintjs/core'
+import { Button, ButtonSize, ButtonVariation, Text } from '@harness/uicore'
+import { Color } from '@harness/design-system'
 import { TemplateStudioInternal } from '@templates-library/components/TemplateStudio/TemplateStudioInternal'
 import { TemplateProvider } from '@templates-library/components/TemplateStudio/TemplateContext/TemplateContext'
 import type {
@@ -17,24 +20,49 @@ import type {
 } from '@common/interfaces/RouteInterfaces'
 import { useQueryParams } from '@common/hooks'
 import { GitSyncStoreProvider } from 'framework/GitRepoStore/GitSyncStoreContext'
+import css from './TemplateStudio.module.scss'
 
 export function TemplateStudio(): React.ReactElement {
   const { accountId, projectIdentifier, orgIdentifier, templateIdentifier, templateType, module } = useParams<
     TemplateStudioPathProps & ModulePathParams
   >()
   const { versionLabel, repoIdentifier, branch } = useQueryParams<TemplateStudioQueryParams & GitQueryParams>()
+  const [showBanner, setShowBanner] = useState(true)
 
   return (
-    <TemplateProvider
-      queryParams={{ accountIdentifier: accountId, orgIdentifier, projectIdentifier, repoIdentifier, branch }}
-      module={module}
-      templateIdentifier={templateIdentifier}
-      versionLabel={versionLabel}
-      templateType={templateType}
-    >
-      <GitSyncStoreProvider>
-        <TemplateStudioInternal />
-      </GitSyncStoreProvider>
-    </TemplateProvider>
+    <>
+      {showBanner && (
+        <Callout className={css.callout} intent="success" icon={null}>
+          <Text color={Color.BLACK}>
+            Harness has made a significant update to the services and environments in CD. Services and Environments now
+            have definitions that are associated with the respective object and can be managed independent of the
+            pipeline. This change is a forward looking change that won’t impact your existing pipelines. However, we do
+            plan on reducing our support on the service v1 and environments v1 and plan to deprecate by end of January.
+            We have an automated migration tool to support our users. Please contact
+            <a href="https://support.harness.io/hc/en-us" target="_blank" rel="noreferrer">
+              <b>&nbsp;support.harness.io&nbsp;</b>
+            </a>
+            for further questions.
+          </Text>
+          <Button
+            variation={ButtonVariation.ICON}
+            size={ButtonSize.LARGE}
+            icon="cross"
+            onClick={() => setShowBanner(false)}
+          />
+        </Callout>
+      )}
+      <TemplateProvider
+        queryParams={{ accountIdentifier: accountId, orgIdentifier, projectIdentifier, repoIdentifier, branch }}
+        module={module}
+        templateIdentifier={templateIdentifier}
+        versionLabel={versionLabel}
+        templateType={templateType}
+      >
+        <GitSyncStoreProvider>
+          <TemplateStudioInternal />
+        </GitSyncStoreProvider>
+      </TemplateProvider>
+    </>
   )
 }
