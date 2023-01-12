0a18836ee39a684da868de3b874618c122e2a222
Sonali Goyal <sonali.goyal@harness.io>
2023-01-12 17:36:03 +0530

    fix: [CDS-50338]: Used setrefvalue prop to retain connector value (#13424)

--- src/modules/70-pipeline/components/PipelineSteps/Steps/JiraCreate/JiraCreateStepMode.tsx
+++ src/modules/70-pipeline/components/PipelineSteps/Steps/JiraCreate/JiraCreateStepMode.tsx
@@ -424,6 +424,7 @@ function FormContent({
               }
             }
           }}
+          setRefValue
           disabled={isApprovalStepFieldDisabled(readonly)}
           gitScope={{ repo: repoIdentifier || '', branch, getDefaultFromOtherRepo: true }}
         />
