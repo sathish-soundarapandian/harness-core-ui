06e67eabd2e88bb46e6383161ce6eb87fd34b1de
sahithikolichala <sahithi.kolichala@harness.io>
2023-01-15 23:45:04 -0800

    feat: [CDS-49722]: adding digest/sha support for docker registry

--- src/modules/75-cd/components/PipelineSteps/TasServiceSpec/__tests__/TasServiceSpec.test.tsx
+++ src/modules/75-cd/components/PipelineSteps/TasServiceSpec/__tests__/TasServiceSpec.test.tsx
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
 import React from 'react'
 import { act, fireEvent, render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
@@ -68,6 +75,7 @@ jest.mock('services/cd-ng', () => ({
   useGetServiceV2: jest.fn().mockImplementation(() => ({ loading: false, data: {}, refetch: jest.fn() })),
   useCreateConnector: jest.fn(() => Promise.resolve(mockCreateConnectorResponse)),
   useGetService: jest.fn().mockImplementation(() => ({ loading: false, data: {}, refetch: jest.fn() })),
+  useGetLastSuccessfulBuildForDockerWithYaml: () => mockDockerTagsCallResponse,
   useUpdateConnector: jest.fn(() =>
     Promise.resolve({
       status: 'SUCCESS',
