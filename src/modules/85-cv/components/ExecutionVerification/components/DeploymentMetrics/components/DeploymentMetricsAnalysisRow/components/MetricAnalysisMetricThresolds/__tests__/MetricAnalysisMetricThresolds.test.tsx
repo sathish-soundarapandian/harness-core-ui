5b414d43f1484c4b3e9f40bbb919f43779e737be
harshilbits <harshil.garg@harness.io>
2023-01-27 18:30:58 +0530

    feat: [SRM-13712]: addressing more checks

--- src/modules/85-cv/components/ExecutionVerification/components/DeploymentMetrics/components/DeploymentMetricsAnalysisRow/components/MetricAnalysisMetricThresolds/__tests__/MetricAnalysisMetricThresolds.test.tsx
+++ src/modules/85-cv/components/ExecutionVerification/components/DeploymentMetrics/components/DeploymentMetricsAnalysisRow/components/MetricAnalysisMetricThresolds/__tests__/MetricAnalysisMetricThresolds.test.tsx
@@ -40,31 +40,95 @@ describe('MetricAnalysisMetricThresolds', () => {
         }
       },
       {
-        id: 'Fh-N1OUnTmmrBWhqqWqJvQ',
+        id: '6L6gbC9oRlCS8ypbtCi0rA',
         thresholdType: 'IGNORE' as MetricThresholdV2['thresholdType'],
+        isUserDefined: false,
+        action: 'FailImmediately' as MetricThresholdV2['action'],
+        criteria: {
+          measurementType: 'ratio' as MetricThresholdCriteriaV2['measurementType'],
+          lessThanThreshold: 0
+        }
+      },
+      {
+        id: '6L6gbC9oRlCS8ypbtCi0rA',
+        thresholdType: 'IGNORE' as MetricThresholdV2['thresholdType'],
+        isUserDefined: false,
+        action: 'FailAfterOccurrence' as MetricThresholdV2['action'],
+        criteria: {
+          measurementType: 'ratio' as MetricThresholdCriteriaV2['measurementType'],
+          lessThanThreshold: 0
+        }
+      },
+      {
+        id: '6L6gbC9oRlCS8ypbtCi0rA',
+        thresholdType: 'IGNORE' as MetricThresholdV2['thresholdType'],
+        isUserDefined: false,
+        action: 'FailAfterConsecutiveOccurrence' as MetricThresholdV2['action'],
+        criteria: {
+          measurementType: 'ratio' as MetricThresholdCriteriaV2['measurementType'],
+          lessThanThreshold: 0
+        }
+      },
+      {
+        id: '6L6gbC9oRlCS8ypbtCi0rA',
+        thresholdType: 'FAIL_FAST' as MetricThresholdV2['thresholdType'],
         isUserDefined: false,
         action: 'Ignore' as MetricThresholdV2['action'],
         criteria: {
           measurementType: 'delta' as MetricThresholdCriteriaV2['measurementType'],
           lessThanThreshold: 0
         }
+      },
+      {
+        id: '6L6gbC9oRlCS8ypbtCi0rA',
+        thresholdType: 'IGNORE' as MetricThresholdV2['thresholdType'],
+        isUserDefined: false,
+        action: 'Ignore' as MetricThresholdV2['action'],
+        criteria: {
+          measurementType: 'absolute-value' as MetricThresholdCriteriaV2['measurementType'],
+          lessThanThreshold: 0
+        }
+      },
+      {
+        id: 'Fh-N1OUnTmmrBWhqqWqJvQ',
+        thresholdType: 'IGNORE' as MetricThresholdV2['thresholdType'],
+        isUserDefined: true,
+        action: 'Ignore' as MetricThresholdV2['action'],
+        criteria: {
+          measurementType: 'delta' as MetricThresholdCriteriaV2['measurementType'],
+          lessThanThreshold: 0
+        }
+      },
+      {
+        id: 'Fh-N1OUnTmmrBWhqqWqJvQ',
+        thresholdType: '',
+        isUserDefined: true,
+        action: '',
+        criteria: {
+          measurementType: '',
+          lessThanThreshold: 0,
+          greaterThanThreshold: 10
+        }
+      },
+      {
+        id: 'Fh-N1OUnTmmrBWhqqWqJvQ',
+        thresholdType: '',
+        isUserDefined: true,
+        action: '',
+        criteria: {
+          measurementType: '',
+          greaterThanThreshold: 100
+        }
       }
-    ]
+    ] as MetricThresholdV2[]
   }
   test('should render threshold table if the data is present', () => {
     const { getByText, queryByTestId } = render(<WrapperComponent {...props} />)
-    // expect columns are present
+    // expect values are present
     expect(getByText('THRESHOLD TYPE')).toBeInTheDocument()
     expect(getByText('CRITERIA')).toBeInTheDocument()
     expect(getByText('VALUE')).toBeInTheDocument()
     expect(getByText('ACTION')).toBeInTheDocument()
     expect(queryByTestId('metric-analysis-metric-threshold')).toBeInTheDocument()
-  })
-
-  test('should not render the table if the threshold data is not present', () => {
-    const newProps = { ...props, thresholds: [] }
-    const { queryByTestId } = render(<WrapperComponent {...newProps} />)
-
-    expect(queryByTestId('metric-analysis-metric-threshold')).not.toBeInTheDocument()
   })
 })
