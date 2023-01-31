5c881adfa57177e425e612a25c82230927a8c931
William Doyle <william.doyle@harness.io>
2023-01-31 14:40:17 +0000

    fix: [IAC-198]: Add iacm overview route

--- src/modules/10-common/RouteDefinitions.ts
+++ src/modules/10-common/RouteDefinitions.ts
@@ -1983,6 +1983,7 @@ const routes = {
 
   /*********************** IACM */
   toIACM: withAccountId(() => `/iacm`),
+  toIACMOverview: withAccountId(() => `/iacm/overview`),
   toIACMMicroFrontend: withAccountId(
     ({ orgIdentifier, projectIdentifier }: Partial<ProjectPathProps>) =>
       `/iacm/orgs/${orgIdentifier}/projects/${projectIdentifier}/`
