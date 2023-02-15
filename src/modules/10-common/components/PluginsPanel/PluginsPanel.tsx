7577ed0c8f8cc1d4babad1c69fa21881f26a1ca0
Vardan Bansal <vardan.bansal@harness.io>
2023-02-14 22:32:22 -0800

    feat: [CI-6287]: fix label

--- src/modules/10-common/components/PluginsPanel/PluginsPanel.tsx
+++ src/modules/10-common/components/PluginsPanel/PluginsPanel.tsx
@@ -176,9 +176,10 @@ export function PluginsPanel(props: PluginsPanelInterface): React.ReactElement {
   const getCertificationLabelForKind = useCallback((_kindOfPlugin: string): string => {
     switch (_kindOfPlugin) {
       case PluginKind.HARNESS:
-      case PluginKind.BITRISE:
       case PluginKind.HARNESS_NATIVE:
         return `by ${capitalize(PluginKind.HARNESS)}`
+      case PluginKind.BITRISE:
+        return `by ${capitalize(_kindOfPlugin)}`
       case PluginKind.ACTION:
         return `by ${getString('common.repo_provider.githubLabel')} ${capitalize(_kindOfPlugin)}`
       default:
