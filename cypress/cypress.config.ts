972e5f135ac7c02a16cdb5a62963ee8554750a3e
Udham Singh <udham.singh@harness.io>
2023-02-22 20:24:27 +0530

    Update cypress.config.ts

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     //baseUrl: 'https://localhost:8181',
-    baseUrl: 'http://director-cy.qa.harness.io/',
+    baseUrl: 'https://director-cy.qa.harness.io/',
     specPattern: 'integration/**/*.spec.{ts,tsx}',
     supportFile: 'support/index.ts',
     fixturesFolder: 'fixtures',
