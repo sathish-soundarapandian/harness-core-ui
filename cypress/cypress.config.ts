b1636dccc42a15113bd881e1ec34c0b7f563cfeb
Udham Singh <udham.singh@harness.io>
2023-02-22 21:13:57 +0530

    Update cypress.config.ts

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'https://localhost:8181',
-    //baseUrl: 'https://director-cy.qa.harness.io/',
     specPattern: 'integration/**/*.spec.{ts,tsx}',
     supportFile: 'support/index.ts',
     fixturesFolder: 'fixtures',
@@ -18,7 +17,6 @@ export default defineConfig({
   },
   fixturesFolder: 'fixtures',
   env: {
-    projectId: 'harness-core-ui',
     // since urls need addHashInCypressURLBasedOnBrowserRouter function to create urls with hash and
     // without hash and inorder to decide if the browserRouterEnabeld  is true or not in app's window object,
     //Cypress does not have access to the app intially so injecting the browserRouterEnabled flag inside cypress env
