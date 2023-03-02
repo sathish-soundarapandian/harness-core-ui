78c59f0ce49fd3cd8cf94fe2846ab8b5bd5e6b7f
sahithikolichala <sahithi.kolichala@harness.io>
2023-03-02 09:53:41 -0800

    fix: [CDS-54545]: modified ui to show imagepath as dropdown for ecr

--- src/modules/72-triggers/components/steps/ArtifactTriggerConfigPanel/ArtifactsSelection/ArtifactRepository/ArtifactLastSteps/ArtifactImagePath/ArtifactImagePath.tsx
+++ src/modules/72-triggers/components/steps/ArtifactTriggerConfigPanel/ArtifactsSelection/ArtifactRepository/ArtifactLastSteps/ArtifactImagePath/ArtifactImagePath.tsx
@@ -5,24 +5,111 @@
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
 
-import React from 'react'
-import { FormInput, MultiTypeInputType } from '@harness/uicore'
+import React, { useEffect, useState } from 'react'
+import { FormInput, MultiSelectOption, MultiTypeInputType, SelectOption } from '@harness/uicore'
+import { useParams } from 'react-router-dom'
+
 import { useStrings } from 'framework/strings'
+
+import { useMutateAsGet, useQueryParams } from '@common/hooks'
+import { GetImagesListForEcrQueryParams, useGetImagesListForEcr } from 'services/cd-ng'
+import type { GitQueryParams, ProjectPathProps } from '@common/interfaces/RouteInterfaces'
+import { EXPRESSION_STRING } from '@pipeline/utils/constants'
 
 import css from '../../ArtifactConnector.module.scss'
 
-function ArtifactImagePath(): React.ReactElement {
+interface ArtifactImagePathProps {
+  connectorRef?: string
+  region?: string
+  artifactType?: string
+}
+
+function ArtifactImagePath(props: ArtifactImagePathProps): React.ReactElement {
   const { getString } = useStrings()
+  const { accountId, projectIdentifier, orgIdentifier } = useParams<ProjectPathProps>()
+  const { repoIdentifier, branch } = useQueryParams<GitQueryParams>()
+  const [images, setImages] = useState<SelectOption[]>([])
+  const { connectorRef, region, artifactType } = props
+
+  const imagesListAPIQueryParams: GetImagesListForEcrQueryParams = {
+    accountIdentifier: accountId,
+    orgIdentifier,
+    projectIdentifier,
+    connectorRef,
+    region: region,
+    repoIdentifier,
+    branch
+  }
+  const {
+    data: imagesListData,
+    loading: imagesListLoading,
+    refetch: refetchImagesList
+    // error: imagesListError
+  } = useMutateAsGet(useGetImagesListForEcr, {
+    requestOptions: {
+      headers: {
+        'content-type': 'application/json'
+      }
+    },
+    queryParams: imagesListAPIQueryParams,
+    lazy: true,
+    debounce: 300
+  })
+
+  useEffect(() => {
+    if (imagesListData?.data) {
+      const imageResponseFormatted: MultiSelectOption[] = imagesListData?.data?.images?.map(
+        (artifactPathVal: string) => {
+          return {
+            label: artifactPathVal,
+            value: artifactPathVal
+          } as MultiSelectOption
+        }
+      ) || [
+        {
+          label: 'No images',
+          value: 'noimages'
+        }
+      ]
+      setImages(imageResponseFormatted)
+    }
+  }, [imagesListData])
+
+  if (artifactType) {
+    return (
+      <div className={css.imagePathContainer}>
+        <FormInput.MultiTypeInput
+          label={getString('pipeline.imagePathLabel')}
+          name="imagePath"
+          useValue
+          selectItems={images}
+          placeholder={getString('pipeline.artifactsSelection.existingDocker.imageNamePlaceholder')}
+          multiTypeInputProps={{
+            allowableTypes: [MultiTypeInputType.FIXED],
+            onFocus: (e: React.FocusEvent<HTMLInputElement>) => {
+              if (
+                e?.target?.type !== 'text' ||
+                (e?.target?.type === 'text' && e?.target?.placeholder === EXPRESSION_STRING)
+              ) {
+                return
+              }
+              if (!imagesListLoading) {
+                refetchImagesList()
+              }
+            }
+          }}
+        />
+      </div>
+    )
+  }
 
   return (
-    <div className={css.imagePathContainer}>
-      <FormInput.MultiTextInput
-        label={getString('pipeline.imagePathLabel')}
-        name="imagePath"
-        placeholder={getString('pipeline.artifactsSelection.existingDocker.imageNamePlaceholder')}
-        multiTextInputProps={{ allowableTypes: [MultiTypeInputType.FIXED] }}
-      />
-    </div>
+    <FormInput.MultiTextInput
+      label={getString('pipeline.imagePathLabel')}
+      name="imagePath"
+      placeholder={getString('pipeline.artifactsSelection.existingDocker.imageNamePlaceholder')}
+      multiTextInputProps={{ allowableTypes: [MultiTypeInputType.FIXED] }}
+    />
   )
 }
 
