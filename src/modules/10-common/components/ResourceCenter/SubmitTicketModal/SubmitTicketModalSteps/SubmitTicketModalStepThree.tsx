704a01292089000dea169d9c4f8b1df952a23c72
Vikrant Gupta <vikrant.gupta@harness.io>
2023-03-04 16:32:07 +0530

    feat: [PL-31801]: integrate components and modules dropdown

--- src/modules/10-common/components/ResourceCenter/SubmitTicketModal/SubmitTicketModalSteps/SubmitTicketModalStepThree.tsx
+++ src/modules/10-common/components/ResourceCenter/SubmitTicketModal/SubmitTicketModalSteps/SubmitTicketModalStepThree.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /*
  * Copyright 2023 Harness Inc. All rights reserved.
  * Use of this source code is governed by the PolyForm Shield 1.0.0 license
@@ -5,13 +6,15 @@
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
 
-import { Layout, Text, Formik, Button, ButtonVariation, StepProps } from '@harness/uicore'
+import { Layout, Text, Formik, Button, ButtonVariation, StepProps, FormInput } from '@harness/uicore'
 import { FontVariation } from '@harness/design-system'
 import * as Yup from 'yup'
 import React from 'react'
 import { Form } from 'formik'
 import { FormMultiTypeTextAreaField } from '@common/components/MultiTypeTextArea/MultiTypeTextArea'
+import { useModuleInfo } from '@common/hooks/useModuleInfo'
 import css from './SubmitTicketModalSteps.module.scss'
+
 interface SubmitTicketModalStepThreeProps {
   name: string
   stepName: string
@@ -25,6 +28,7 @@ export const SubmitTicketModalStepThree = (props: StepProps<any> & SubmitTicketM
   const fileUploadHandler = (event: any): void => {
     setFileData(event.target.files[0])
   }
+  const { module } = useModuleInfo()
 
   return (
     <Layout.Vertical height={'inherit'} spacing="medium" className={css.optionsViewContainer}>
@@ -32,7 +36,11 @@ export const SubmitTicketModalStepThree = (props: StepProps<any> & SubmitTicketM
         {stepName}
       </Text>
       <Formik
-        initialValues={defaultInitialValues}
+        initialValues={{
+          ticketDetails: '',
+          module: module,
+          component: ''
+        }}
         formName="ticketDetailsForm"
         validationSchema={Yup.object().shape({
           ticketDetails: Yup.string().required('Ticket Details are required')
@@ -42,12 +50,46 @@ export const SubmitTicketModalStepThree = (props: StepProps<any> & SubmitTicketM
           onCloseHandler()
         }}
       >
-        {() => (
+        {formik => (
           <Form>
             <Layout.Vertical
               flex={{ justifyContent: 'space-between', alignItems: 'flex-start' }}
               className={css.manifestForm}
             >
+              <Layout.Horizontal spacing="medium">
+                <FormInput.MultiTypeInput
+                  name="module"
+                  label={'Module'}
+                  className={css.inputWidth}
+                  placeholder="Select the Module with the issue"
+                  selectItems={moduleOptions}
+                  useValue
+                  multiTypeInputProps={{
+                    selectProps: {
+                      items: moduleOptions,
+                      allowCreatingNewItems: true,
+                      addClearBtn: true
+                    }
+                  }}
+                />
+              </Layout.Horizontal>
+              <Layout.Horizontal spacing="medium">
+                <FormInput.MultiTypeInput
+                  name="component"
+                  label={'Component'}
+                  className={css.inputWidth}
+                  placeholder="Select the Components with the issue"
+                  selectItems={getComponentsFromModule(formik.values.module as string)}
+                  useValue
+                  multiTypeInputProps={{
+                    selectProps: {
+                      items: getComponentsFromModule(formik.values.module as string),
+                      allowCreatingNewItems: true,
+                      addClearBtn: true
+                    }
+                  }}
+                />
+              </Layout.Horizontal>
               <Layout.Horizontal spacing="medium">
                 <FormMultiTypeTextAreaField
                   name="ticketDetails"
@@ -59,13 +101,15 @@ export const SubmitTicketModalStepThree = (props: StepProps<any> & SubmitTicketM
               <Layout.Horizontal spacing="medium">
                 <input name="fileData" type="file" onChange={fileUploadHandler} />
               </Layout.Horizontal>
-              <Button
-                variation={ButtonVariation.PRIMARY}
-                type="submit"
-                text={'Submit'}
-                rightIcon="chevron-right"
-                className={css.saveBtn}
-              />
+              <Layout.Horizontal spacing="medium">
+                <Button
+                  variation={ButtonVariation.PRIMARY}
+                  type="submit"
+                  text={'Submit'}
+                  rightIcon="chevron-right"
+                  className={css.saveBtn}
+                />
+              </Layout.Horizontal>
             </Layout.Vertical>
           </Form>
         )}
@@ -74,6 +118,31 @@ export const SubmitTicketModalStepThree = (props: StepProps<any> & SubmitTicketM
   )
 }
 
-const defaultInitialValues = {
-  ticketDetails: ''
+const moduleOptions = [
+  {
+    label: 'CD',
+    value: 'cd'
+  },
+  {
+    label: 'CI',
+    value: 'ci'
+  },
+  {
+    label: 'CV',
+    value: 'cv'
+  }
+]
+
+export const getComponentsFromModule = (module: string): Array<{ label: string; value: string }> => {
+  switch (module) {
+    case 'cd':
+      return [
+        { label: 'Pipeline', value: 'Pipeline' },
+        { label: 'InputSets', value: 'InputSets' }
+      ]
+    case 'ci':
+      return [{ label: 'Build', value: 'Build' }]
+    default:
+      return [{ label: 'Misc', value: 'Misc' }]
+  }
 }
