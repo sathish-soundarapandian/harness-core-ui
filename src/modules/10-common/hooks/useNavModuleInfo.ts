922f61705a1f21e78a22c38e65bd6c9fd60b1a98
dhruv upadhyay <dhruv.upadhyay@harness.io>
2023-03-17 17:56:46 +0530

    fix: [PLG-2071]: Removed license check condition for showing modules … (#14685)
    * fix: [PLG-2071]: Removed license check condition for showing modules in nav
    
    * fix: updated test cases
    
    * fix: Updated test cases
    
    * fix: [PLG-2071]: updating snapshots
    
    * fix: [PLG-2071]: updating snapshots
    
    ---------
    
    Co-authored-by: Avinash Madhwani <avinash.madhwani@harness.io>

--- src/modules/10-common/hooks/useNavModuleInfo.ts
+++ src/modules/10-common/hooks/useNavModuleInfo.ts
@@ -218,9 +218,9 @@ const shouldBeVisible = (
     )
   }
 
-  return featureFlagName !== undefined
+  return featureFlagName !== undefined && featureFlags[featureFlagName] !== undefined
     ? !!featureFlags[featureFlagName]
-    : licenseInformation[module]?.status === 'ACTIVE'
+    : true
 }
 
 const useNavModuleInfo = (module: NavModuleName) => {
