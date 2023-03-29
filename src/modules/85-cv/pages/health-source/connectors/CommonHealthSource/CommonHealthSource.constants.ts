ad4b429e532359a92c559ba4f3471d21862cd2b9
harshilbits <harshil.garg@harness.io>
2023-03-29 20:56:20 +0530

    feat: [SRM-14389]: hotfix for elk

--- src/modules/85-cv/pages/health-source/connectors/CommonHealthSource/CommonHealthSource.constants.ts
+++ src/modules/85-cv/pages/health-source/connectors/CommonHealthSource/CommonHealthSource.constants.ts
@@ -1,4 +1,3 @@
-import type { QueryRecordsRequest } from 'services/cv'
 import { ElkProduct } from '../../HealthSourceDrawer/component/defineHealthSource/DefineHealthSource.constant'
 import { HealthSourceTypes } from '../../types'
 import type { CommonCustomMetricFormikInterface, HealthSourceProductsType } from './CommonHealthSource.types'
