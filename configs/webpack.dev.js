dedd62a3d06c320e72915b9b6ed476ab49c7504a
Pratyush Garg <pratyush.garg@harness.io>
2023-04-07 00:41:53 +0530

    fix: test

--- configs/webpack.dev.js
+++ configs/webpack.dev.js
@@ -87,7 +87,7 @@ const config = {
         historyApiFallback: {
           disableDotRule: true
         },
-        port: 8182,
+        port: 8181,
         client: {
           overlay: !(isCypress || isCypressCoverage)
         },
