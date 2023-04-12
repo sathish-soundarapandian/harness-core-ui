9d7c7b8c3e17f19da1ab5cde7503ed5ea9e3dd3f
ishantmehtaharness <ishant@harness.io>
2023-04-12 23:55:04 +0530

    Pretty Text

--- src/modules/70-pipeline/pages/pipeline-intelligence/Assistant.tsx
+++ src/modules/70-pipeline/pages/pipeline-intelligence/Assistant.tsx
@@ -16,6 +16,34 @@ import { useGetTemplates } from 'services/pipeline-ng'
 import css from '@freeze-windows/components/FreezeWindowStudioBody/FreezeWindowStudioBody.module.scss'
 import { Spinner } from '@blueprintjs/core'
 
+function PrettyText({ text }) {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: text
+      }}
+    />
+  )
+}
+
+function prettyRender(text) {
+  if (!text) return ''
+  text = text.replace(/\s{2,}/g, ' ').trim()
+  text = text.replace(/(\*{1,3})\s+/g, '$1')
+  text = text.replace(/\s+(\*{1,3})/g, '$1')
+  text = text.replace(/(\|)\s+/g, '$1')
+  text = text.replace(/\s+(\|)/g, '$1')
+  text = text.replace(/\n/g, ' ')
+  text = text.replace(/(\.)(\s+)(\w)/g, '$1\n\n$3')
+  text = text.replace(/(\:)(\s+)(\|)/g, '$1\n\n$3')
+  text = text.replace(/(Reference:)/g, '\n\n$1')
+  text = text.replace(/(See the following:)/g, '\n\n$1')
+  text = text.replace(/(\#\s+Before You Begin)/g, '\n\n$1')
+  text = text.replace(/(\#\#\s+Stages)/g, '\n\n$1')
+  text = text.replace(/(\#\#\s+Pipelines)/g, '\n\n$1')
+  text = text.replace(/(http(s)?:\/\/[^\s]+)/g, '<a href="$1">$1</a>')
+  return text
+}
 export default function Assistant(props) {
   const [query, setQuery] = React.useState('')
   const [questionAndAnswers, setQuestionAndAnswers] = React.useState([
@@ -29,15 +57,12 @@ export default function Assistant(props) {
     lazy: true
   })
 
-  let isFirstTime = true
-
   React.useEffect(() => {
-    if (isFirstTime) {
-      isFirstTime = false
-      return
-    }
     if (!loading) {
-      setQuestionAndAnswers(items => [...items.slice(0, -1), { question: items.at(-1).question, answer: data?.data }])
+      setQuestionAndAnswers(items => [
+        ...items.slice(0, -1),
+        { question: items.at(-1).question, answer: prettyRender(data?.data) || items.at(-1).answer }
+      ])
     }
   }, [loading])
 
@@ -84,7 +109,7 @@ export default function Assistant(props) {
                         font={{ size: 'medium', weight: '500' }}
                         margin={{ top: '5px' }}
                       >
-                        {qNa.answer}
+                        <PrettyText text={qNa.answer} />
                       </Text>
                     )}
                   </Layout.Vertical>
