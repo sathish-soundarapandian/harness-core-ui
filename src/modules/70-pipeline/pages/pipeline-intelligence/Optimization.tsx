e44b8b74bfcad37a0511fb2fcfc02174c872b230
ishantmehtaharness <ishant@harness.io>
2023-04-12 18:42:45 +0530

    make initial tabs

--- src/modules/70-pipeline/pages/pipeline-intelligence/Optimization.tsx
+++ src/modules/70-pipeline/pages/pipeline-intelligence/Optimization.tsx
@@ -1,5 +1,94 @@
 import React from 'react'
+import {
+  Button,
+  ButtonVariation,
+  Color,
+  Container,
+  FontVariation,
+  Formik,
+  FormikForm,
+  FormInput,
+  Layout,
+  Text
+} from '@harness/uicore'
+import { noop } from 'lodash-es'
+import { useMutateAsGet } from '@common/hooks'
+import { useGetPipelineList } from '../../../../services/pipeline-ng'
+import { prepareFiltersPayload } from '@pipeline/pages/utils/Filters/filters'
+import { getFilterByIdentifier } from '@pipeline/utils/PipelineExecutionFilterRequestUtils'
+import { useParams } from 'react-router-dom'
+import { PipelineListPagePathParams } from '@pipeline/pages/pipeline-list/types'
 
 export default function Optimization(props) {
-  return <div>Optimization</div>
+  const requestBody = {
+    filterType: 'PipelineSetup'
+  }
+
+  const pathParams = useParams<PipelineListPagePathParams>()
+  const { projectIdentifier, orgIdentifier, accountId, module } = pathParams
+
+  const pipelinesQuery = useMutateAsGet(useGetPipelineList, {
+    body: requestBody,
+    queryParams: {
+      accountIdentifier: accountId,
+      projectIdentifier,
+      orgIdentifier,
+      searchTerm: '',
+      page: 0,
+      sort: `lastUpdatedAt DESC`,
+      size: 100
+    },
+    queryParamStringifyOptions: { arrayFormat: 'comma' },
+    requestOptions: {
+      headers: {
+        'content-type': 'application/json'
+      }
+    }
+  })
+
+  const onSubmit = values => {
+    const firstPipeline = values.firstPipeline
+    const secondPipeline = values.secondPipeline
+    // Make API call to compare both
+  }
+
+  return (
+    <Layout.Vertical flex={{ justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+      <Layout.Horizontal>
+        <Formik enableReinitialize onSubmit={noop} formName={'Standardization'} initialValues={{}}>
+          {formikProps => {
+            return (
+              <FormikForm>
+                <FormInput.Select
+                  name={'firstPipeline'}
+                  items={
+                    pipelinesQuery.loading
+                      ? []
+                      : pipelinesQuery.data?.data?.content?.map(a => ({ label: a.name, value: a.identifier })) || []
+                  }
+                  placeholder={'Select First Pipeline'}
+                  label={'Select First Pipeline'}
+                  style={{ width: '400px' }}
+                />
+                <FormInput.Select
+                  name={'secondPipeline'}
+                  items={
+                    pipelinesQuery.loading
+                      ? []
+                      : pipelinesQuery.data?.data?.content?.map(a => ({ label: a.name, value: a.identifier })) || []
+                  }
+                  placeholder={'Select Second Pipeline'}
+                  label={'Select Second Pipeline'}
+                  style={{ width: '400px' }}
+                />
+                <Container margin={{ top: 'xxlarge' }}>
+                  <Button onClick={onSubmit} variation={ButtonVariation.PRIMARY} text={'Compare and see the magic'} />
+                </Container>
+              </FormikForm>
+            )
+          }}
+        </Formik>
+      </Layout.Horizontal>
+    </Layout.Vertical>
+  )
 }
--- src/modules/70-pipeline/pages/pipeline-intelligence/Standardization.tsx
+++ src/modules/70-pipeline/pages/pipeline-intelligence/Standardization.tsx
@@ -1,5 +1,82 @@
 import React from 'react'
+import {
+  Button,
+  ButtonVariation,
+  Color,
+  Container,
+  FontVariation,
+  Formik,
+  FormikForm,
+  FormInput,
+  Layout,
+  Text
+} from '@harness/uicore'
+import { noop } from 'lodash-es'
+import { useMutateAsGet } from '@common/hooks'
+import { useGetPipelineList } from '../../../../services/pipeline-ng'
+import { prepareFiltersPayload } from '@pipeline/pages/utils/Filters/filters'
+import { getFilterByIdentifier } from '@pipeline/utils/PipelineExecutionFilterRequestUtils'
+import { useParams } from 'react-router-dom'
+import { PipelineListPagePathParams } from '@pipeline/pages/pipeline-list/types'
 
 export default function Standardization(props) {
-  return <div>Standardization</div>
+  const requestBody = {
+    filterType: 'PipelineSetup'
+  }
+
+  const pathParams = useParams<PipelineListPagePathParams>()
+  const { projectIdentifier, orgIdentifier, accountId, module } = pathParams
+
+  const pipelinesQuery = useMutateAsGet(useGetPipelineList, {
+    body: requestBody,
+    queryParams: {
+      accountIdentifier: accountId,
+      projectIdentifier,
+      orgIdentifier,
+      searchTerm: '',
+      page: 0,
+      sort: `lastUpdatedAt DESC`,
+      size: 100
+    },
+    queryParamStringifyOptions: { arrayFormat: 'comma' },
+    requestOptions: {
+      headers: {
+        'content-type': 'application/json'
+      }
+    }
+  })
+
+  const onSubmit = values => {
+    const goldenPipeline = values.goldenPipeline
+    // Make API call to
+  }
+
+  return (
+    <Layout.Vertical flex={{ justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+      <Layout.Horizontal>
+        <Formik enableReinitialize onSubmit={noop} formName={'Standardization'} initialValues={{}}>
+          {formikProps => {
+            return (
+              <FormikForm>
+                <FormInput.Select
+                  name={'goldenPipeline'}
+                  items={
+                    pipelinesQuery.loading
+                      ? []
+                      : pipelinesQuery.data?.data?.content?.map(a => ({ label: a.name, value: a.identifier })) || []
+                  }
+                  placeholder={'Select your Golder Pipeline'}
+                  label={'Select your Golder Pipeline'}
+                  style={{ width: '400px' }}
+                />
+                <Container margin={{ top: 'xxlarge' }}>
+                  <Button onClick={onSubmit} variation={ButtonVariation.PRIMARY} text={'Save'} />
+                </Container>
+              </FormikForm>
+            )
+          }}
+        </Formik>
+      </Layout.Horizontal>
+    </Layout.Vertical>
+  )
 }
