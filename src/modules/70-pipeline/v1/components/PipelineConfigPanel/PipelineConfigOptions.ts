a08099615c7b28481b06ad6d1610309ce8f1e413
Vardan Bansal <vardan.bansal@harness.io>
2023-04-19 15:00:57 -0700

    feat: [CI-7766]: handling config option select - detailed view

--- src/modules/70-pipeline/v1/components/PipelineConfigPanel/PipelineConfigOptions.ts
+++ src/modules/70-pipeline/v1/components/PipelineConfigPanel/PipelineConfigOptions.ts
@@ -1,6 +1,7 @@
 import type { IconProps } from '@harness/icons'
 
 export interface PipelineConfigOptionInterface {
+  entity: PipelineEntity
   label: string
   iconProps: IconProps
   description: string
@@ -19,20 +20,61 @@ export const enum PipelineEntity {
 }
 
 export const MainConfigOptions: PipelineConfigOptionInterface[] = [
-  { label: 'Stages', iconProps: { name: 'add-stage', size: 20 }, description: 'Add a stage' },
-  { label: 'Triggers', iconProps: { name: 'yaml-builder-trigger', size: 20 }, description: 'Add a trigger' },
-  { label: 'Notifications', iconProps: { name: 'notifications', size: 20 }, description: 'Add a notification' },
-  { label: 'Inputs', iconProps: { name: 'template-inputs', size: 20 }, description: 'Add an input' }
+  {
+    entity: PipelineEntity.Stage,
+    label: 'Stages',
+    iconProps: { name: 'add-stage', size: 20 },
+    description: 'Add a stage'
+  },
+  {
+    entity: PipelineEntity.Trigger,
+    label: 'Triggers',
+    iconProps: { name: 'yaml-builder-trigger', size: 20 },
+    description: 'Add a trigger'
+  },
+  {
+    entity: PipelineEntity.Notification,
+    label: 'Notifications',
+    iconProps: { name: 'notifications', size: 20 },
+    description: 'Add a notification'
+  },
+  {
+    entity: PipelineEntity.Input,
+    label: 'Inputs',
+    iconProps: { name: 'template-inputs', size: 20 },
+    description: 'Add an input'
+  }
 ]
 
 export const AdditionalConfigOptions: PipelineConfigOptionInterface[] = [
-  { label: 'Barriers', iconProps: { name: 'command-barrier', size: 20 }, description: 'Add a barrier' },
-  { label: 'Clone', iconProps: { name: 'code-clone', size: 20 }, description: 'Add a codebase clone' },
-  { label: 'Delegates', iconProps: { name: 'main-delegates', size: 20 }, description: 'Add a delegate' },
   {
+    entity: PipelineEntity.Barrier,
+    label: 'Barriers',
+    iconProps: { name: 'command-barrier', size: 20 },
+    description: 'Add a barrier'
+  },
+  {
+    entity: PipelineEntity.Clone,
+    label: 'Clone',
+    iconProps: { name: 'code-clone', size: 20 },
+    description: 'Add a codebase clone'
+  },
+  {
+    entity: PipelineEntity.Delegate,
+    label: 'Delegates',
+    iconProps: { name: 'main-delegates', size: 20 },
+    description: 'Add a delegate'
+  },
+  {
+    entity: PipelineEntity.EnvVariable,
     label: 'Environment Variables',
     iconProps: { name: 'pipeline-variables', size: 20 },
     description: 'Add an environment variable'
   },
-  { label: 'Registry', iconProps: { name: 'azure-container-registry', size: 20 }, description: 'Add a registry' }
+  {
+    entity: PipelineEntity.Registry,
+    label: 'Registry',
+    iconProps: { name: 'azure-container-registry', size: 20 },
+    description: 'Add a registry'
+  }
 ]
--- src/modules/70-pipeline/v1/components/PipelineConfigPanel/PipelineConfigPanel.tsx
+++ src/modules/70-pipeline/v1/components/PipelineConfigPanel/PipelineConfigPanel.tsx
@@ -7,9 +7,23 @@ interface PipelineConfigPanelInterface {
   height?: React.CSSProperties['height']
 }
 
+enum PipelineConfigPanelView {
+  Options = 'OPTIONS',
+  ConfigureOption = 'CONFIGURE_OPTION'
+}
+
 export function PipelineConfigPanel(props: PipelineConfigPanelInterface): React.ReactElement {
   const { height } = props
   const [showMoreOptions, setShowMoreOptions] = useState<boolean>(false)
+  const [selectedConfigOption, setSelectedConfigOption] = useState<PipelineConfigOptionInterface>()
+  const [pipelineConfigPanelView, setPipelineConfigPanelView] = useState<PipelineConfigPanelView>(
+    PipelineConfigPanelView.Options
+  )
+
+  const onSelectConfigOption = useCallback((selectedConfigOption: PipelineConfigOptionInterface): void => {
+    setSelectedConfigOption(selectedConfigOption)
+    setPipelineConfigPanelView(PipelineConfigPanelView.ConfigureOption)
+  }, [])
 
   const renderPipelineConfigOption = useCallback((configOption: PipelineConfigOptionInterface): React.ReactElement => {
     const { label, iconProps, description } = configOption
@@ -19,6 +33,7 @@ export function PipelineConfigPanel(props: PipelineConfigPanelInterface): React.
         padding={{ top: 'medium', bottom: 'medium' }}
         flex={{ justifyContent: 'space-between' }}
         className={css.configOption}
+        onClick={() => onSelectConfigOption(configOption)}
       >
         <Layout.Horizontal flex>
           <Icon {...iconProps} />
@@ -32,24 +47,54 @@ export function PipelineConfigPanel(props: PipelineConfigPanelInterface): React.
     )
   }, [])
 
+  const renderPipelineConfigOptions = useCallback((): React.ReactElement => {
+    return (
+      <>
+        <Container
+          padding={{ left: 'xlarge', top: 'medium', bottom: 'medium', right: 'xlarge' }}
+          className={css.header}
+        >
+          <Text font={{ variation: FontVariation.H4 }}>Pipeline Configuration</Text>
+        </Container>
+        <Layout.Vertical padding={{ left: 'xxlarge', right: 'xxlarge', top: 'xxxlarge' }}>
+          {(showMoreOptions ? [...MainConfigOptions, ...AdditionalConfigOptions] : MainConfigOptions).map(
+            (option: PipelineConfigOptionInterface) => renderPipelineConfigOption(option)
+          )}
+        </Layout.Vertical>
+        {showMoreOptions ? null : (
+          <Container
+            padding={{ left: 'xlarge', top: 'small', bottom: 'medium', right: 'xlarge' }}
+            flex={{ justifyContent: 'center' }}
+          >
+            <Button
+              text={'SEE MORE OPTIONS'}
+              variation={ButtonVariation.LINK}
+              onClick={() => setShowMoreOptions(true)}
+            />
+          </Container>
+        )}
+      </>
+    )
+  }, [showMoreOptions])
+
+  const renderPipelineConfigOptionDetails = useCallback((): React.ReactElement => {
+    return <>{selectedConfigOption?.label}</>
+  }, [selectedConfigOption])
+
+  const renderPipelineConfigPanel = useCallback((): React.ReactElement => {
+    switch (pipelineConfigPanelView) {
+      case PipelineConfigPanelView.Options:
+        return renderPipelineConfigOptions()
+      case PipelineConfigPanelView.ConfigureOption:
+        return renderPipelineConfigOptionDetails()
+      default:
+        return <></>
+    }
+  }, [showMoreOptions, pipelineConfigPanelView])
+
   return (
     <Layout.Vertical style={{ height }} width="100%">
-      <Container padding={{ left: 'xlarge', top: 'medium', bottom: 'medium', right: 'xlarge' }} className={css.header}>
-        <Text font={{ variation: FontVariation.H4 }}>Pipeline Configuration</Text>
-      </Container>
-      <Layout.Vertical padding={{ left: 'xxlarge', right: 'xxlarge', top: 'xxxlarge' }}>
-        {(showMoreOptions ? [...MainConfigOptions, ...AdditionalConfigOptions] : MainConfigOptions).map(
-          (option: PipelineConfigOptionInterface) => renderPipelineConfigOption(option)
-        )}
-      </Layout.Vertical>
-      {showMoreOptions ? null : (
-        <Container
-          padding={{ left: 'xlarge', top: 'small', bottom: 'medium', right: 'xlarge' }}
-          flex={{ justifyContent: 'center' }}
-        >
-          <Button text={'SEE MORE OPTIONS'} variation={ButtonVariation.LINK} onClick={() => setShowMoreOptions(true)} />
-        </Container>
-      )}
+      {renderPipelineConfigPanel()}
     </Layout.Vertical>
   )
 }
