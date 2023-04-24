c40643c77c58b9eb3f9b872e0a00ac229aa10c34
dhruv upadhyay <dhruv.upadhyay@harness.io>
2023-04-24 19:38:10 +0530

    feat: [PLG-1569]: updating test coverage

--- src/modules/33-auth-settings/pages/subscriptions/__tests__/BUildCreditInfo.test.tsx
+++ src/modules/33-auth-settings/pages/subscriptions/__tests__/BUildCreditInfo.test.tsx
@@ -8,7 +8,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { TestWrapper } from '@common/utils/testUtils'
-import { ModuleName } from 'framework/types/ModuleName'
 import { BuildCreditInfoTable } from '../overview/BuildCreditInfoTable'
 
 enum Edition {
@@ -55,5 +54,30 @@ describe('Build Credit Info test ', () => {
     )
 
     expect(container).toMatchSnapshot('build credit table')
+  })
+  test('Build Credit Info with emmpty credit data', () => {
+    const data = {
+      id: '622596d705bb66724f02cf12',
+      accountIdentifier: 'zEaak-FLS425IEO7OLzMUg',
+      moduleType: ModuleType.CI,
+      edition: Edition.ENTERPRISE,
+      licenseType: LicenseType.PAID,
+      status: Status.ACTIVE,
+      startTime: 1633977619398,
+      expiryTime: 1766494425000,
+      premiumSupport: false,
+      selfService: false,
+      createdAt: 1646630615316,
+      lastModifiedAt: 1646630615316,
+      cdLicenseType: 'SERVICES',
+      workloads: 100
+    }
+    const { container } = render(
+      <TestWrapper>
+        <BuildCreditInfoTable licenseData={data} data={[]} />
+      </TestWrapper>
+    )
+
+    expect(container).toMatchSnapshot('build credit table with empty credit data')
   })
 })
