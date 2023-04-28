867ce202b45c51db57aedabc703687b875366185
Abhinav Rastogi <abhinav.rastogi@harness.io>
2023-04-28 14:47:40 +0530

    send token and put support

--- src/modules/10-common/pages/CorsTestPage.tsx
+++ src/modules/10-common/pages/CorsTestPage.tsx
@@ -1,15 +1,24 @@
 import React, { useRef, useState } from 'react'
+import SessionToken from 'framework/utils/SessionToken'
 
 export default function CorsTestPage(): JSX.Element {
   const ref = useRef(null)
   const bodyRef = useRef(null)
+  const tokenRef = useRef(null)
   const [method, setMethod] = useState('GET')
   const handleSubmit = (ev): void => {
     ev.preventDefault()
     ev.stopPropagation()
+    const sendToken = tokenRef?.current?.checked
+
+    const headers = {}
+    if (sendToken) {
+      headers['Authorization'] = `Bearer ${SessionToken.getToken()}`
+    }
 
     fetch(ref.current?.value, {
       method,
+      headers,
       body: method === 'POST' ? JSON.stringify(bodyRef.current.value) : void 0
     })
       .then(res => res.text())
@@ -29,16 +38,24 @@ export default function CorsTestPage(): JSX.Element {
         >
           <option>GET</option>
           <option>POST</option>
+          <option>PUT</option>
+          <option>DELETE</option>
         </select>
         <input
           type="text"
           name="url"
           style={{ width: '500px' }}
-          placeholder="https://pr.harness.io/<namespace>/api/<endpoint>"
+          defaultValue={'https://app.harness.io/gateway/pipeline/api/pipelines/list'}
           ref={ref}
         />
       </div>
-      <div>{method === 'POST' ? <textarea name="body" ref={bodyRef} placeholder={'{}'} /> : null}</div>
+      <div>
+        <input type="checkbox" ref={tokenRef} defaultChecked={true} id="sendToken" />
+        <label htmlFor="sendToken">Send Token</label>
+      </div>
+      <div>
+        {method === 'POST' || method === 'PUT' ? <textarea name="body" ref={bodyRef} defaultValue={'{}'} /> : null}
+      </div>
       <div>
         <button type="submit">Submit</button>
       </div>
