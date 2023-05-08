9bb8c93165f01ac4aafaf07c562ab9ec35c54e16
Avinash Madhwani <avinash.madhwani@harness.io>
2023-05-08 23:19:21 +0530

    feat: [PL-32366]: Multiple IDP changes (#15636)
    * feat: [PL-32366]: Multiple IDP changes
    
    * fix: type fix
    
    * fix: fixed types

---
+++ src/modules/33-auth-settings/modals/SAMLProvider/steps/SelectProvider.tsx
@@ -0,0 +1,113 @@
+import React from 'react'
+import {
+  StepProps,
+  Layout,
+  Button,
+  ButtonVariation,
+  Label,
+  ThumbnailSelect,
+  FormikForm,
+  Text,
+  Heading
+} from '@harness/uicore'
+import { Color, FontVariation } from '@harness/design-system'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+import { useStrings } from 'framework/strings'
+import { FormValues, Providers, SAMLProviderType } from '../utils'
+import css from '../useSAMLProvider.module.scss'
+
+interface SelectProviderForm {
+  samlProviderType?: Providers
+}
+
+const SelectProvider: React.FC<StepProps<FormValues>> = props => {
+  const { getString } = useStrings()
+
+  const SAMLProviderTypes: SAMLProviderType[] = [
+    {
+      value: Providers.AZURE,
+      label: getString('authSettings.azure'),
+      icon: 'service-azure'
+    },
+    {
+      value: Providers.OKTA,
+      label: getString('authSettings.okta'),
+      icon: 'service-okta'
+    },
+    {
+      value: Providers.ONE_LOGIN,
+      label: getString('authSettings.oneLogin'),
+      icon: 'service-onelogin'
+    },
+    {
+      value: Providers.OTHER,
+      label: getString('common.other'),
+      icon: 'main-more'
+    }
+  ]
+
+  return (
+    <Layout.Horizontal height="100%">
+      <Formik<SelectProviderForm>
+        initialValues={{
+          samlProviderType: props.prevStepData?.samlProviderType
+        }}
+        validationSchema={Yup.object().shape({
+          samlProviderType: Yup.string().trim().required(getString('common.validation.nameIsRequired'))
+        })}
+        onSubmit={values => {
+          props.nextStep?.({ ...props.prevStepData, ...values } as FormValues)
+        }}
+      >
+        <FormikForm className={css.form}>
+          <Layout.Vertical>
+            <Text margin={{ bottom: 'large' }} font={{ variation: FontVariation.H3 }}>
+              {getString('authSettings.selectProvider')}
+            </Text>
+            <Layout.Vertical
+              spacing="small"
+              flex={{ justifyContent: 'space-between', alignItems: 'flex-start' }}
+              className={css.flex}
+            >
+              <Layout.Vertical>
+                <Label>{getString('authSettings.selectSAMLProvider')}</Label>
+                <ThumbnailSelect name="samlProviderType" items={SAMLProviderTypes} />
+              </Layout.Vertical>
+              <Layout.Horizontal padding={{ top: 'small' }} spacing="medium">
+                <Button
+                  text={getString('back')}
+                  icon="chevron-left"
+                  variation={ButtonVariation.SECONDARY}
+                  onClick={() => props?.previousStep?.({ ...props.prevStepData } as FormValues)}
+                  data-name="awsBackButton"
+                />
+                <Button
+                  type="submit"
+                  variation={ButtonVariation.PRIMARY}
+                  text={getString('continue')}
+                  rightIcon="chevron-right"
+                />
+              </Layout.Horizontal>
+            </Layout.Vertical>
+          </Layout.Vertical>
+        </FormikForm>
+      </Formik>
+      <Layout.Vertical
+        width={290}
+        padding={{ left: 'xxxlarge', top: 'xxxlarge' }}
+        margin={{ bottom: 'large' }}
+        border={{ left: true }}
+      >
+        <Heading level={6} color={Color.BLACK} font={{ weight: 'semi-bold' }} margin={{ bottom: 'medium' }}>
+          {getString('authSettings.friendlyReminder')}
+        </Heading>
+        <Text color={Color.GREY_800} font={{ size: 'small' }} margin={{ bottom: 'xxlarge' }} className={css.notes}>
+          {getString('authSettings.friendlyReminderDescription')}
+        </Text>
+      </Layout.Vertical>
+    </Layout.Horizontal>
+  )
+}
+
+export default SelectProvider
--- src/stringTypes.ts
+++ src/stringTypes.ts
@@ -375,6 +375,8 @@ export interface StringsMap {
   'common.freezeWindows': string
   'common.frequency': string
   'common.friendlyMessage': string
+  'common.friendlyName': string
+  'common.friendlyNamePlaceholder': string
   'common.gcp': string
   'common.genericErrors.inviteExpired': string
   'common.genericErrors.licenseCallFailed': string
@@ -2043,6 +2045,7 @@ export interface StringsMap {
   'authSettings.accountOrOAuthLogin': string
   'authSettings.accountOrOAuthLoginEnabledSuccessfully': string
   'authSettings.addSAMLProvider': string
+  'authSettings.additionalFunctions': string
   'authSettings.allowLoginFromTheseDomains': string
   'authSettings.allowUsersWithEmails': string
   'authSettings.atLeastNChars': string
@@ -2183,6 +2186,7 @@ export interface StringsMap {
   'authSettings.haveOneSpecialChar': string
   'authSettings.haveOneUppercase': string
   'authSettings.identityProvider': string
+  'authSettings.identityProviderLabel': string
   'authSettings.keepAtLeastOneProviderEnabled': string
   'authSettings.ldap.addGroupQueryDescription': string
   'authSettings.ldap.addGroupQueryHeading': string
@@ -2260,6 +2264,7 @@ export interface StringsMap {
   'authSettings.multipleOrganizations': string
   'authSettings.multipleProjects': string
   'authSettings.newMembersWillNeedToSetUpTwoFADuringSignup': string
+  'authSettings.newSamlProvider': string
   'authSettings.notifyUsersWhenTheyLocked': string
   'authSettings.notifyUsersWhenUserLocked': string
   'authSettings.oauthSettingsHaveBeenUpdated': string
@@ -2295,10 +2300,12 @@ export interface StringsMap {
   'authSettings.samlLoginEnabled': string
   'authSettings.samlProviderAddedSuccessfully': string
   'authSettings.samlProviderDeleted': string
+  'authSettings.samlProviderOverview': string
   'authSettings.samlProviderUpdatedSuccessfully': string
   'authSettings.samlSupport': string
   'authSettings.samlTestFailed': string
   'authSettings.samlTestSuccessful': string
+  'authSettings.selectProvider': string
   'authSettings.selectSAMLProvider': string
   'authSettings.sessionTimOutSaved': string
   'authSettings.sessionTimeOut': string
@@ -2319,6 +2326,7 @@ export interface StringsMap {
   'authSettings.unitPrice': string
   'authSettings.updating': string
   'authSettings.uploadIdentityProvider': string
+  'authSettings.urlIdentityProvider': string
   'authSettings.useHarnessUsernameAndPassword': string
   'authSettings.usePublicOAuth': string
   'authSettings.whitelistedDomainsDisabled': string
