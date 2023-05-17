b7f0df4d60a312dbf714e1e31dd5d08e05ea13e8
Kevin Nagurski <Kevin.nagurski@harness.io>
2023-05-17 11:32:52 +0100

    chore: [FFM-7672]: Hotfix 0.348.x (#15827)
    * feat: [FFM-7672]: Adding Runtime/Expressions to Flag Config (#15514)
    * feat: [FFM-7864]: Temporarily removing expressions from Flag config (#15778)
    * chore: [FFM-7672]: Bump version
    
    ---------
    
    Co-authored-by: laura-mannus <103498167+laura-mannus@users.noreply.github.com>

--- src/modules/75-cf/components/PipelineSteps/FlagConfigurationStep/FlagConfigurationStep.tsx
+++ src/modules/75-cf/components/PipelineSteps/FlagConfigurationStep/FlagConfigurationStep.tsx
@@ -7,7 +7,7 @@
 
 import React, { ReactElement } from 'react'
 import * as Yup from 'yup'
-import { RUNTIME_INPUT_VALUE } from '@harness/uicore'
+import { MultiTypeInputType, RUNTIME_INPUT_VALUE } from '@harness/uicore'
 import type { IconName } from '@harness/icons'
 import { FormikErrors, yupToFormErrors } from 'formik'
 import { isEmpty, set } from 'lodash-es'
@@ -90,6 +90,7 @@ export class FlagConfigurationStep extends PipelineStep<FlagConfigurationStepDat
         isNewStep={isNewStep}
         readonly={readonly}
         ref={formikRef}
+        allowableTypes={[MultiTypeInputType.FIXED, MultiTypeInputType.RUNTIME]}
       />
     )
   }
